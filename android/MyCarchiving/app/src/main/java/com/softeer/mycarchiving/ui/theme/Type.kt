73c87252713a1af754f8817b873f1a41b8c5c099
SeungWoo-Ahn <rio970319@gmail.com>
2023-08-02 18:14:58 +0900

    #1 res: Font 기본 컬러 추가

--- android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/ui/theme/Type.kt
+++ android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/ui/theme/Type.kt
@@ -17,117 +17,134 @@ val bold26 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W700,
     fontSize = 26.sp,
-    lineHeight = 36.sp
+    lineHeight = 36.sp,
+    color = Black
 )
 
 val bold24= TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W700,
     fontSize = 24.sp,
-    lineHeight = 32.sp
+    lineHeight = 32.sp,
+    color = Black
 )
 
 val bold20 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W700,
     fontSize = 20.sp,
-    lineHeight = 28.sp
+    lineHeight = 28.sp,
+    color = Black
 )
 
 val bold18 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W700,
     fontSize = 18.sp,
-    lineHeight = 24.sp
+    lineHeight = 24.sp,
+    color = Black
 )
 
 val medium26 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W500,
     fontSize = 26.sp,
-    lineHeight = 36.sp
+    lineHeight = 36.sp,
+    color = Black
 )
 
 val medium24 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W500,
     fontSize = 24.sp,
-    lineHeight = 32.sp
+    lineHeight = 32.sp,
+    color = Black
 )
 
 val medium20 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W500,
     fontSize = 20.sp,
-    lineHeight = 28.sp
+    lineHeight = 28.sp,
+    color = Black
 )
 
 val medium18 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W500,
     fontSize = 18.sp,
-    lineHeight = 24.sp
+    lineHeight = 24.sp,
+    color = Black
 )
 
 val medium16 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W500,
     fontSize = 16.sp,
-    lineHeight = 24.sp
+    lineHeight = 24.sp,
+    color = Black
 )
 
 val medium14 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W500,
     fontSize = 14.sp,
-    lineHeight = 22.sp
+    lineHeight = 22.sp,
+    color = Black
 )
 
 val medium12 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W500,
     fontSize = 12.sp,
-    lineHeight = 20.sp
+    lineHeight = 20.sp,
+    color = Black
 )
 
 val medium10 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W500,
     fontSize = 10.sp,
-    lineHeight = 20.sp
+    lineHeight = 20.sp,
+    color = Black
 )
 
 val regular18 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W400,
     fontSize = 18.sp,
-    lineHeight = 24.sp
+    lineHeight = 24.sp,
+    color = Black
 )
 
 val regular16 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W400,
     fontSize = 16.sp,
-    lineHeight = 24.sp
+    lineHeight = 24.sp,
+    color = Black
 )
 
 val regular14 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W400,
     fontSize = 14.sp,
-    lineHeight = 22.sp
+    lineHeight = 22.sp,
+    color = Black
 )
 
 val regular12 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W400,
     fontSize = 12.sp,
-    lineHeight = 20.sp
+    lineHeight = 20.sp,
+    color = Black
 )
 
 val regular10 = TextStyle(
     fontFamily = hyundaiFontFamily,
     fontWeight = FontWeight.W400,
     fontSize = 10.sp,
-    lineHeight = 20.sp
+    lineHeight = 20.sp,
+    color = Black
 )
