53e7621d456fffe1a03c48203f3f893d8f7ebe5a
lee-yun-pyo <yun2193@hanmail.net>
2023-08-02 21:03:32 +0900

    chore: Header 폴더명 변경

--- frontend/src/components/common/header/index.tsx
+++ frontend/src/components/common/header/index.tsx
@@ -1,1 +1,30 @@
-export { Header } from './Header';
+import { HyundaiLogo } from '../hyundaiLogo/index.tsx';
+import { ArchivingLogo } from '../ArchivingLogo/index.tsx';
+import { AutoSavingLogo } from '../AutoSavingLogo/index.tsx';
+import * as style from './style';
+
+export function Header() {
+  return (
+    <style.Container>
+      <style.Wrapper>
+        <HyundaiLogo />
+        <style.Division />
+        <style.Text>내차 만들기</style.Text>
+      </style.Wrapper>
+      <style.ButtonWrapper>
+        <style.InfoBox>
+          <style.AutoSavingBox>
+            <style.AutoSavingText>자동저장 중</style.AutoSavingText>
+            <AutoSavingLogo />
+          </style.AutoSavingBox>
+          <style.CarNameText>팰리세이드</style.CarNameText>
+        </style.InfoBox>
+        <style.Division />
+        <style.ButtonBox>
+          <ArchivingLogo />
+          <style.ButtonText>아카이빙</style.ButtonText>
+        </style.ButtonBox>
+      </style.ButtonWrapper>
+    </style.Container>
+  );
+}
--- frontend/src/components/templates/trim/TrimTemplate.tsx
+++ frontend/src/components/templates/trim/TrimTemplate.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@components/common/header/Header';
+import { Header } from '@components/common/Header/index';
 
 export function TrimTemplate() {
   return (
