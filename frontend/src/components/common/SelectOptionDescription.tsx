e46c8937aa0edf1aa3989c69c8838c647dbf8761
sukam09 <sukam09@naver.com>
2023-08-02 17:54:28 +0900

    feat: SelectOptionDescription 컴포넌트 구현

---
+++ frontend/src/components/common/SelectOptionDescription.tsx
@@ -0,0 +1,28 @@
+import * as style from './SelectOptionDescription.style';
+
+interface SelectOptionDescriptionProps {
+  title: string;
+  price: string;
+  tags: string[];
+}
+
+export function SelectOptionDescription({ title, price, tags }: SelectOptionDescriptionProps) {
+  return (
+    <style.Container>
+      <style.TitleWrapper>
+        <style.TitleBox>{title}</style.TitleBox>
+        <style.PriceBox>+{price}원</style.PriceBox>
+      </style.TitleWrapper>
+      <style.Line />
+      <style.DescriptionWrapper>
+        <style.SubTitleBox>{title}</style.SubTitleBox>
+        <style.MessageBox>에 대해 시승자들은 이런 후기를 남겼어요</style.MessageBox>
+      </style.DescriptionWrapper>
+      <style.TagWrapper>
+        {tags.map(tag => (
+          <style.TagBox key={tag}>{tag}</style.TagBox>
+        ))}
+      </style.TagWrapper>
+    </style.Container>
+  );
+}
--- frontend/src/components/templates/trim/TrimTemplate.tsx
+++ frontend/src/components/templates/trim/TrimTemplate.tsx
@@ -1,5 +1,15 @@
 import { SelectOptionImageBox } from '@components/common/SelectOptionImageBox';
+import { SelectOptionDescription } from '@components/common/SelectOptionDescription';
 
 export function TrimTemplate() {
-  return <SelectOptionImageBox image="src/assets/hmg.png" />;
+  return (
+    <>
+      <SelectOptionImageBox image="src/assets/hmg.png" />
+      <SelectOptionDescription
+        title="컴포트 ||"
+        price="690,000"
+        tags={['여름에 쓰기 좋아요☀️', '옵션값 뽑았어요👍', '편리해요☺️']}
+      />
+    </>
+  );
 }
