bb90ca06a1588150c91418342280f976ea75a13e
SeungWoo-Ahn <rio970319@gmail.com>
2023-08-02 11:31:57 +0900

    #1 config: 프로젝트 실행을 위한 코드 정리

--- android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/MainActivity.kt
+++ android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/MainActivity.kt
@@ -3,6 +3,7 @@ package com.softeer.mycarchiving
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.softeer.domain.MyScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
