6cf8e3cfd8dd238e46162087ad65c2eac77d4a14
sukam09 <sukam09@naver.com>
2023-08-03 01:20:02 +0900

    feat: SelectOptionDetail 컴포넌트 구현

--- frontend/src/components/common/SelectOptionDetail/style.tsx
+++ frontend/src/components/common/SelectOptionDetail/style.tsx
@@ -8,8 +8,10 @@ export const Container = styled.div`
     return css`
       width: 507px;
       height: 152px;
+      padding: 24px 32px;
 
       display: flex;
+      flex-direction: column;
 
       border-radius: 8px;
       border: 2px solid ${colors.hyundaiPrimaryBlue};
@@ -19,20 +21,118 @@ export const Container = styled.div`
 `;
 
 export const TitleWrapper = styled.div`
-  width: 507px;
+  width: 443px;
   height: 152px;
 
   display: flex;
-  flex-direction: column;
+  justify-content: space-between;
 `;
 
 export const TitleBox = styled.div`
+  gap: 8px;
+
+  display: flex;
+`;
+
+export const Ellipse = styled.div`
+  ${({ theme }) => {
+    const { colors } = theme;
+
+    return css`
+      width: 22px;
+      height: 22px;
+      padding-right: 0.5px;
+
+      font-family: ‘HyundaiSansBold’;
+      color: ${colors.hyundaiLightSand};
+      text-align: center;
+      font-size: 12px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 20px;
+      letter-spacing: -0.6px;
+
+      border-radius: 50%;
+      border: 2px solid #fff;
+      background-color: ${colors.hyundaiPrimaryBlue};
+    `;
+  }}
+`;
+
+export const Title = styled.div`
   ${({ theme }) => {
     const { colors, fonts } = theme;
 
     return css`
+      height: 24px;
+
       color: ${colors.hyundaiPrimaryBlue};
       ${fonts.headingMedium4}
+    `;
+  }}
+`;
+
+export const OrderBox = styled.div`
+  ${({ theme }) => {
+    const { colors } = theme;
+
+    return css`
+      width: 33px;
+      height: 17px;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      border-radius: 13px;
+      background-color: ${colors.darkGray};
+    `;
+  }}
+`;
+
+export const Order = styled.p`
+  ${({ theme }) => {
+    const { colors, fonts } = theme;
+
+    return css`
+      height: 100%;
+
+      color: ${colors.hyundaiLightSand};
+      ${fonts.captionRegular}
+    `;
+  }}
+`;
+
+export const Line = styled.hr`
+  ${({ theme }) => {
+    const { colors } = theme;
+
+    return css`
+      width: 443px;
+      height: 1px;
+
+      background-color: ${colors.hyundaiPrimaryBlue};
+    `;
+  }}
+`;
+
+export const DescriptionWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+`;
+
+export const Description = styled.div`
+  ${({ theme }) => {
+    const { colors, fonts } = theme;
+
+    return css`
+      width: 380px;
+      height: 61px;
+
+      color: ${colors.hyundaiPrimaryBlue};
+      ${fonts.bodyRegular3}
     `;
   }}
 `;
