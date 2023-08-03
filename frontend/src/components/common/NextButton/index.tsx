a968d12d46491aa9933e7a86c58c65d30edcc2c2
lee-yun-pyo <yun2193@hanmail.net>
2023-08-03 23:03:08 +0900

    feat: NextButton 구현

---
+++ frontend/src/components/common/NextButton/index.tsx
@@ -0,0 +1,30 @@
+import * as style from './style';
+
+interface NextButtonProps {
+  width: string;
+  height: string;
+  onClick: () => void;
+}
+
+export function NextButton({ width, height, onClick }: NextButtonProps) {
+  return (
+    <style.Container width={+width} height={+height} onClick={() => onClick()}>
+      <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 48 48" fill="none">
+        <g clipPath="url(#clip0_577_11602)">
+          <path
+            d="M17 36L29 24L17 12"
+            stroke="#002C5F"
+            strokeOpacity="0.6"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_577_11602">
+            <rect width={width} height={height} fill="white" transform="matrix(-1 0 0 1 48 0)" />
+          </clipPath>
+        </defs>
+      </svg>
+    </style.Container>
+  );
+}
