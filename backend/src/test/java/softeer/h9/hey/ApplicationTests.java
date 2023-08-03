103e04d8ef013748dc63b3cd3b8638db660d9d8a
윤상현 <yshyeon102@gmail.com>
2023-08-03 17:15:11 +0900

    config: 스프링 프로젝트 세팅

---
+++ backend/src/test/java/softeer/h9/hey/ApplicationTests.java
@@ -0,0 +1,13 @@
+package softeer.h9.hey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
