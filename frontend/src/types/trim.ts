02e988e30e7f14cce62437564e0ef3e51db80d02
kim-limit <kimjaehhan@gmail.com>
2023-08-03 21:25:09 +0900

    #23 feat: trim페이지 type 구현

---
+++ frontend/src/types/trim.ts
@@ -0,0 +1,28 @@
+export interface TrimOptionType {
+  title: string;
+  price: number;
+  images: string[];
+}
+
+export interface BodyTypeOptionType {
+  title: string;
+  price: number;
+  description: string;
+  images: string[];
+}
+
+export interface WheelDriveOptionType {
+  title: string;
+  price: number;
+  description: string;
+  image: string;
+}
+
+export interface EngineOptionType {
+  title: string;
+  price: number;
+  description: string;
+  power: string;
+  torque: string;
+  image: string;
+}
