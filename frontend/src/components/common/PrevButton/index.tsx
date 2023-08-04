b9f3792ab5aca75cfa287c12997cc5c56716261a
lee-yun-pyo <yun2193@hanmail.net>
2023-08-04 11:42:45 +0900

    refactor: parseInt 적용

--- frontend/src/components/common/PrevButton/index.tsx
+++ frontend/src/components/common/PrevButton/index.tsx
@@ -8,7 +8,7 @@ interface PrevButtonProps {
 
 export function PrevButton({ width, height, onClick }: PrevButtonProps) {
   return (
-    <style.Container width={+width} height={+height} onClick={() => onClick()}>
+    <style.Container width={parseInt(width)} height={parseInt(height)} onClick={() => onClick()}>
       <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} viewBox="0 0 48 48" fill="none">
         <g clipPath="url(#clip0_577_11601)">
           <path
