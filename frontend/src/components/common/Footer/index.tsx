a0d3eff1bd3c07473d5559ed8b73f7e06c6a02f1
kim-limit <kimjaehhan@gmail.com>
2023-08-05 15:59:58 +0900

    #23 feat: 페이지 전환 및 로컬스토리지 저장 기능 추가

---
+++ frontend/src/components/common/Footer/index.tsx
@@ -0,0 +1,94 @@
+import { useLocation, useNavigate } from 'react-router-dom';
+
+import * as style from './style';
+import { MyCarType } from '@/types/trim';
+
+const NAVIGATION_PATH = {
+  '/trim': { prev: '', next: '/trim/engine' },
+  '/trim/engine': { prev: '/trim', next: '/trim/body-type' },
+  '/trim/body-type': { prev: '/trim/engine', next: '/trim/wheel-drive' },
+  '/trim/wheel-drive': { prev: '/trim/body-type', next: 'color' },
+  '/color': { prev: '/trim/wheel-drive', next: '/option' },
+  '/option': { prev: '/color', next: '/option/h-genuine-accessories' },
+  '/option/h-genuine-accessories': { prev: '/option', next: '/option/n-performance' },
+  '/option/n-performance': { prev: '/option/h-genuine-accessories', next: '' },
+};
+
+interface FooterProps {
+  options: MyCarType;
+  totalPrice: number;
+  onSetLocalStorage: () => void;
+}
+export function Footer({
+  options: { model, engine, bodyType, wheelDrive, color, options },
+  totalPrice,
+  onSetLocalStorage,
+}: FooterProps) {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+
+  const trim = `${engine.title}${bodyType.title !== '' ? '/' : ''}${bodyType.title}${
+    wheelDrive.title !== '' ? '/' : ''
+  }${wheelDrive.title}`;
+
+  const handleNextNavigate = () => {
+    const path = NAVIGATION_PATH[pathname as keyof typeof NAVIGATION_PATH].next;
+    if (path !== '') {
+      onSetLocalStorage();
+      navigate(NAVIGATION_PATH[pathname as keyof typeof NAVIGATION_PATH].next);
+    }
+  };
+  const handlePrevNavigate = () => {
+    const path = NAVIGATION_PATH[pathname as keyof typeof NAVIGATION_PATH].prev;
+    path !== '' && navigate(NAVIGATION_PATH[pathname as keyof typeof NAVIGATION_PATH].prev);
+  };
+
+  return (
+    <style.Container>
+      <style.TrimWrapper>
+        <style.Title>트림</style.Title>
+        <style.CarName>{model.title}</style.CarName>
+        <style.TrimDetail>{trim}</style.TrimDetail>
+      </style.TrimWrapper>
+      <style.Division />
+      <style.ColorWrapper>
+        <style.Title>선택 색상</style.Title>
+        <style.ColorBox>
+          <style.ColorTitle>외장</style.ColorTitle>
+          <style.ColorName>
+            <style.ColorCircle />
+            <style.ColorNameText>{color.inner[1]}</style.ColorNameText>
+          </style.ColorName>
+        </style.ColorBox>
+        <style.ColorBox>
+          <style.ColorTitle>내장</style.ColorTitle>
+          <style.ColorName>
+            <style.ColorCircle />
+            <style.ColorNameText>{color.outer[1]}</style.ColorNameText>
+          </style.ColorName>
+        </style.ColorBox>
+      </style.ColorWrapper>
+      <style.Division />
+      <style.OptionWrapper>
+        <style.Title>선택 옵션</style.Title>
+        <style.OptionBox>
+          {options.map(option => (
+            <style.Option>{option}</style.Option>
+          ))}
+        </style.OptionBox>
+      </style.OptionWrapper>
+      <style.Division />
+      <style.PriceWrapper>
+        <style.Title>예상 가격</style.Title>
+        <style.PriceText>
+          {totalPrice.toLocaleString()}
+          <style.PriceUnitText>원</style.PriceUnitText>
+        </style.PriceText>
+      </style.PriceWrapper>
+      <style.ButtonWrapper>
+        <style.PrevButton onClick={handlePrevNavigate}>이전</style.PrevButton>
+        <style.NextButton onClick={handleNextNavigate}>다음</style.NextButton>
+      </style.ButtonWrapper>
+    </style.Container>
+  );
+}
