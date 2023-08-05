81ba8f0783d3aeb7ce32701fed423ee77dff068d
sukam09 <sukam09@naver.com>
2023-08-06 03:37:23 +0900

    chore: 사용하지 않는 interface 주석 제거

--- frontend/src/types/option.ts
+++ frontend/src/types/option.ts
@@ -18,22 +18,3 @@ export interface SelectOptionCardDataProps {
   imageUrl: string;
   subOptionNames: string[];
 }
-
-// export interface OptionDetailsProps {
-//   title: string;
-//   description: string;
-// }
-
-// export interface SelectOptionCardInfoProps {
-//   image: string;
-//   title: string;
-//   price: number;
-//   descriptions: string[];
-// }
-
-// export interface DefaultOptionCardInfoProps {
-//   image: string;
-//   text: string;
-//   subtext: string;
-//   description: string;
-// }
