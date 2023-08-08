ac63be0984e8448ba27e8b68bd9806500cacc4a6
lee-yun-pyo <yun2193@hanmail.net>
2023-08-08 16:31:45 +0900

    #52 fix: 외부 색상 클릭 시 에러 수정

--- frontend/src/pages/Color/index.tsx
+++ frontend/src/pages/Color/index.tsx
@@ -125,6 +125,7 @@ export function Color() {
 
   function handleClickExternalColor(index: number) {
     handleSetExternalIndex(index)();
+    handleSetInnerIndex(0)();
     setIsExternalPage(true);
   }
 
