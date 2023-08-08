f63c29f9a78a50076136ef164e9e138085248e21
kim-limit <kimjaehhan@gmail.com>
2023-08-08 20:05:12 +0900

    #54 refactor: api 데이터 및 경로 수정

--- frontend/src/mocks/api/trim-h-genuine-accessories/data.ts
+++ frontend/src/mocks/api/trim-h-genuine-accessories/data.ts
@@ -1,27 +1,31 @@
 export const data = {
   selectOptions: [
     {
+      id: 1,
       name: '듀얼 머플러 패키지',
-      price: 10900000,
-      imageUrl: '/path',
+      imageURL: '/path',
       tags: ['좋아요', '굳', '태그 5개'],
       subOptions: [
         {
+          id: 1,
           name: '듀얼 머플러 패키지',
-          imageUrl: '/path',
+          additionalPrice: 123000,
+          imageURL: '/path',
           description: '-',
         },
       ],
     },
     {
+      id: 1,
       name: '빌트인 공기청정기',
-      price: 10900000,
-      imageUrl: '/path',
+      additionalPrice: 10900000,
+      imageURL: '/path',
       tags: ['좋아요', '굳', '태그 5개'],
       subOptions: [
         {
+          id: 1,
           name: '빌트인 공기청정기',
-          imageUrl: 'path',
+          imageURL: 'path',
           description:
             '※ 빌트인 공기청정기 전용 에어필터의 권장 사용기간은 6개월이며(하루 2시간 사용 기준), 에어필터는 현대 Shop(Shop.Hyundai.com) 현대브랜드관을 통해 개별 품목 단위로 구매 가능합니다.',
         },
