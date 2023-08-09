120ee9f8ba6016dc1da746cf523553c9698e8f37
윤상현 <yshyeon102@gmail.com>
2023-08-09 14:07:26 +0900

    #57 feat: 모든 API에서 공통으로 사용하는 GlobalResponse Dto 구현

---
+++ backend/src/main/java/softeer/h9/hey/dto/response/GlobalResponse.java
@@ -0,0 +1,28 @@
+package softeer.h9.hey.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@Builder
+public class GlobalResponse<T>  {
+
+    private int statusCode;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T data;
+
+    public static <T> GlobalResponse<T> ok(T body) {
+        return GlobalResponse.<T>builder()
+                .statusCode(HttpStatus.OK.value())
+                .data(body)
+                .build();
+    }
+}
