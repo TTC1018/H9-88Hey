ac19b328968d95cec8d2824ede81894d66de9e48
윤상현 <yshyeon102@gmail.com>
2023-08-09 23:02:06 +0900

    #56 feat: 엔진 도메인 클래스 구현

---
+++ backend/src/main/java/softeer/h9/hey/domain/car/Engine.java
@@ -0,0 +1,20 @@
+package softeer.h9.hey.domain.car;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Engine {
+    private int id;
+    private int additionalPrice;
+    private int modelId;
+    private String name;
+    private String imageUrl;
+    private String description;
+    private String maximumPower;
+    private String maximumTorque;
+}
