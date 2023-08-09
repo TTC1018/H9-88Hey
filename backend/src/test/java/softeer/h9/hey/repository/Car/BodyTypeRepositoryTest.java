bc59a2b2ac788e9fb8225c48d728580d7f4318cb
ddingmin <ddingminyt@gmail.com>
2023-08-09 23:07:30 +0900

    #10 test: 모델 id 기준 바디타입을 조회하는 BodyTypeRepository 테스트 구현

---
+++ backend/src/test/java/softeer/h9/hey/repository/Car/BodyTypeRepositoryTest.java
@@ -0,0 +1,28 @@
+package softeer.h9.hey.repository.Car;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import softeer.h9.hey.domain.car.BodyType;
+
+import java.util.List;
+
+@SpringBootTest
+@DisplayName("BodyType Repository 테스트")
+class BodyTypeRepositoryTest {
+
+    @Autowired
+    private BodyTypeRepository bodyTypeRepository;
+
+    @Test
+    @DisplayName("model id에 해당하는 바디 타입을 모두 조회한다")
+    void getBodyTypesByModelId() {
+        int modelId = 1;
+
+        List<BodyType> bodyTypes = bodyTypeRepository.findAllByModelId(modelId);
+
+        Assertions.assertThat(bodyTypes).isNotNull();
+    }
+}
