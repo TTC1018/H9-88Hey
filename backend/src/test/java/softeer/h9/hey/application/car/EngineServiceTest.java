5adeec434930ced06202d2d4ba8ae26580be3378
윤상현 <yshyeon102@gmail.com>
2023-08-09 22:17:57 +0900

    #56 test: 엔진 서비스로부터 model ID를 통해 엔진을 검색하는 기능 테스트 추가

---
+++ backend/src/test/java/softeer/h9/hey/application/car/EngineServiceTest.java
@@ -0,0 +1,32 @@
+package softeer.h9.hey.application.car;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import softeer.h9.hey.domain.car.Engine;
+import softeer.h9.hey.dto.car.response.EnginesResponse;
+import softeer.h9.hey.repository.car.EngineRepository;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+class EngineServiceTest {
+
+    EngineRepository engineRepository = Mockito.mock(EngineRepository.class);
+    EngineService engineService = new EngineService(engineRepository);
+
+    @Test
+    void searchEngineTest() {
+        int modelId = 1;
+        Engine engine1 = new Engine(1, 10000, modelId, "name", "imageURL", "descprition", "maximumPower", "maximumTorque");
+        Engine engine2 = new Engine(2, 10000, modelId, "name", "imageURL", "descprition", "maximumPower", "maximumTorque");
+        when(engineRepository.findByModelId(modelId)).thenReturn(List.of(engine1, engine2));
+
+        EnginesResponse enginesResponse = engineService.findEnginesByModelId(modelId);
+        List<Engine> engines = enginesResponse.getEngines();
+
+        Assertions.assertThat(engines).hasSize(2);
+        Assertions.assertThat(engines).containsExactly(engine1, engine2);
+    }
+}
