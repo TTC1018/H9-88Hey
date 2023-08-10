fc7c969b08c01b1d4c9212056c2ab58c8bc5d361
윤상현 <yshyeon102@gmail.com>
2023-08-10 22:26:09 +0900

    #64 feat: CarCode 가져오기 기능 구현

---
+++ backend/src/main/java/softeer/h9/hey/service/car/CarInfoService.java
@@ -0,0 +1,28 @@
+package softeer.h9.hey.service.car;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import softeer.h9.hey.domain.car.CarInfo;
+import softeer.h9.hey.dto.car.request.CarCodeRequest;
+import softeer.h9.hey.dto.car.response.CarCodeResponse;
+import softeer.h9.hey.repository.car.CarInfoRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+public class CarInfoService {
+
+    private final CarInfoRepository carInfoRepository;
+
+    public CarCodeResponse getCarCode(CarCodeRequest carCodeRequest) {
+        int trimId = carCodeRequest.getTrimId();
+        int engineId = carCodeRequest.getEngineId();
+        int bodyTypeId = carCodeRequest.getBodyTypeId();
+        int wheelDriveId = carCodeRequest.getWheelDriveId();
+
+        Optional<CarInfo> optionalCarInfo = carInfoRepository.findBy(trimId, engineId, bodyTypeId, wheelDriveId);
+        CarInfo carInfo = optionalCarInfo.orElseThrow(() -> new RuntimeException("Not Found That CarCode"));
+        return CarCodeResponse.of(carInfo.getCarCode());
+    }
+}
