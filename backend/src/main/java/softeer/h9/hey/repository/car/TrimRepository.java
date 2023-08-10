3ceb5e66640a92c23a72e9f58c488e633dc2f252
윤상현 <yshyeon102@gmail.com>
2023-08-10 12:00:35 +0900

    #63 feat: DB에서 트림을 조회하는 기능 구현
    트림 검색시 연관된 트림 특징들도 함께 가져와서 매핑해주도록 구현

---
+++ backend/src/main/java/softeer/h9/hey/repository/car/TrimRepository.java
@@ -0,0 +1,50 @@
+package softeer.h9.hey.repository.car;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import softeer.h9.hey.domain.car.Trim;
+import softeer.h9.hey.domain.car.TrimFeature;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+@RequiredArgsConstructor
+public class TrimRepository {
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public List<Trim> findTrimsByModelId(int modelId) {
+        String sql = "SELECT * FROM trim LEFT JOIN trim_feature tf ON trim.id = tf.trim_id WHERE trim.model_id = :model_id";
+
+        Map<String, Object> params = Map.of("model_id", modelId);
+
+        return fetchTrimsWithFeatures(sql, params);
+    }
+
+    private ArrayList<Trim> fetchTrimsWithFeatures(String sql, Map<String, Object> params) {
+        HashMap<Integer, Trim> trimMap = new HashMap<>();
+
+        namedParameterJdbcTemplate.query(sql, params, rs -> {
+            int trimId = rs.getInt("trim.id");
+
+            Trim trim = trimMap.getOrDefault(
+                    trimId,
+                    new Trim(trimId, rs.getString("trim.name"), rs.getInt("price"))
+            );
+
+            TrimFeature trimFeature = new TrimFeature();
+            trimFeature.setName(rs.getString("tf.name"));
+            trimFeature.setImageURL(rs.getString("tf.image_url"));
+
+            trim.addTrimFeature(trimFeature);
+
+            trimMap.putIfAbsent(trimId, trim);
+        });
+
+        return new ArrayList<>(trimMap.values());
+    }
+}
