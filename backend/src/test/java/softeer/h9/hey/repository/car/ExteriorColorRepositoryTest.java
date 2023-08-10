3091c822a08c2ce2321029f0b97325feec32616d
ddingmin <ddingminyt@gmail.com>
2023-08-10 23:56:31 +0900

    #72 test: 외장 색상 조회 repository 테스트 작성

---
+++ backend/src/test/java/softeer/h9/hey/repository/car/ExteriorColorRepositoryTest.java
@@ -0,0 +1,35 @@
+package softeer.h9.hey.repository.car;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import softeer.h9.hey.domain.car.ExteriorColor;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@DisplayName("ExteriorColorRepository Test")
+class ExteriorColorRepositoryTest {
+
+    @Autowired
+    private ExteriorColorRepository exteriorColorRepository;
+
+    @Test
+    @DisplayName("DB에 저장된 특정 트림의 외장 색상들을 조회한다.")
+    void findAllByTrimId() {
+        int trimId = 1;
+
+        List<ExteriorColor> exteriorColors = exteriorColorRepository.findAllByTrimId(trimId);
+
+        assertAll(
+                () -> assertNotNull(exteriorColors),
+                () -> assertEquals(6, exteriorColors.size()),
+                () -> exteriorColors.forEach(exteriorColor ->
+                        assertEquals(trimId, exteriorColor.getTrimId())
+                )
+        );
+    }
+}
