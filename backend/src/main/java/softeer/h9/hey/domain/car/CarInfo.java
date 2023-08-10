fa4b177540d9d834f6b4e8a32e966f5b715fd236
윤상현 <yshyeon102@gmail.com>
2023-08-10 22:22:19 +0900

    #64 feat: carInfo 클래스에 생성자 추가

--- backend/src/main/java/softeer/h9/hey/domain/car/CarInfo.java
+++ backend/src/main/java/softeer/h9/hey/domain/car/CarInfo.java
@@ -1,10 +1,12 @@
 package softeer.h9.hey.domain.car;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class CarInfo {
     private String carCode;
     private int trimId;
