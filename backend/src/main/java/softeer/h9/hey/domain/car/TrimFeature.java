0cafc3dc2d5b56a26a6cbc812597f05aa023ea0b
윤상현 <yshyeon102@gmail.com>
2023-08-10 12:23:12 +0900

    #63 feat: TrimFeature 클래스의 생성자 추가

--- backend/src/main/java/softeer/h9/hey/domain/car/TrimFeature.java
+++ backend/src/main/java/softeer/h9/hey/domain/car/TrimFeature.java
@@ -1,10 +1,14 @@
 package softeer.h9.hey.domain.car;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TrimFeature {
     private String name;
     private String imageURL;
