b44281c260f919c7e3d1ab064e4e5eeeebbe64f7
RecordOfJun <pritty0731@naver.com>
2023-08-10 14:05:14 +0900

    #65 feat: ModelImageController 모델 이미지 조회 기능 작성

--- backend/src/main/java/softeer/h9/hey/controller/car/ModelImageController.java
+++ backend/src/main/java/softeer/h9/hey/controller/car/ModelImageController.java
@@ -1,4 +1,21 @@
 package softeer.h9.hey.controller.car;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import softeer.h9.hey.dto.car.response.EnginesResponse;
+import softeer.h9.hey.dto.global.response.GlobalResponse;
+
+@RestController
+@RequiredArgsConstructor
 public class ModelImageController {
+
+    private final ModelImageService modelImageService;
+
+    @GetMapping("/car/model/{modelId}/Image")
+    public GlobalResponse<ModelImageResponse> findImageByModelId(@PathVariable final int modelId) {
+        ModelImageResponse modelResponse = modelImageService.findImageByModelId(modelId);
+        return GlobalResponse.ok(ModelImageResponse);
+    }
 }
