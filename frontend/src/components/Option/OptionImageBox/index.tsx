a231038fb65c09293387c2ac3d0aeb9170f5dab3
sukam09 <sukam09@naver.com>
2023-08-12 03:00:51 +0900

    #66 feat: 옵션 위치 보기 버튼 제거

--- frontend/src/components/Option/OptionImageBox/index.tsx
+++ frontend/src/components/Option/OptionImageBox/index.tsx
@@ -8,7 +8,6 @@ export function OptionImageBox({ imageUrl }: OptionImageBoxProps) {
   return (
     <style.Container>
       <style.Image src={imageUrl} />
-      <style.Button>옵션 위치 보기</style.Button>
     </style.Container>
   );
 }
