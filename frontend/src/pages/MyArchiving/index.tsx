9f4840547bb0bf1558f78f7f2db183662b43174f
lee-yun-pyo <yun2193@hanmail.net>
2023-08-11 10:07:19 +0900

    #73 feat: 마이카이빙 UI 구현

--- frontend/src/pages/MyArchiving/index.tsx
+++ frontend/src/pages/MyArchiving/index.tsx
@@ -1,11 +1,26 @@
+import { MyCarList } from '@/components/archiving/MyCarList';
 import { ArchivingHeader } from '@/components/common/ArchivingHeader';
 import { ArchivingNavigation } from '@/components/common/ArchivingNavigation';
+
+import * as style from './style';
 
 export function MyArchiving() {
   return (
     <>
       <ArchivingHeader />
       <ArchivingNavigation />
+      <style.Container>
+        <style.Wrapper>
+          <style.TitleText>내가 만든 차량 목록</style.TitleText>
+          <style.Division />
+          <style.MyCarBox>
+            <MyCarList />
+            <MyCarList />
+            <MyCarList />
+            <MyCarList />
+          </style.MyCarBox>
+        </style.Wrapper>
+      </style.Container>
     </>
   );
 }
---
+++ frontend/src/pages/MyArchiving/style.tsx
@@ -0,0 +1,45 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const Container = styled.main`
+  width: 100vw;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Wrapper = styled.div`
+  width: 1024px;
+`;
+
+export const TitleText = styled.h3`
+  ${({ theme }) => {
+    const { colors, fonts } = theme;
+    return css`
+      ${fonts.headingMedium3};
+      color: ${colors.black};
+    `;
+  }}
+`;
+
+export const Division = styled.div`
+  ${({ theme }) => {
+    const { colors } = theme;
+    return css`
+      margin: 8px 0 25px 0;
+
+      width: 100%;
+      height: 6px;
+
+      background-color: ${colors.hyundaiLightSand};
+    `;
+  }}
+`;
+
+export const MyCarBox = styled.div`
+  gap: 20px;
+
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
