def384b42a08a5fd032639679b9873913372b5bf
ddingmin <ddingminyt@gmail.com>
2023-08-12 14:18:37 +0900

    #72 feat: 변경된 내장색상 도메인에 맞게 수정

--- backend/src/main/java/softeer/h9/hey/repository/car/InteriorColorRepository.java
+++ backend/src/main/java/softeer/h9/hey/repository/car/InteriorColorRepository.java
@@ -1,15 +1,16 @@
 package softeer.h9.hey.repository.car;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
-import softeer.h9.hey.domain.car.InteriorColor;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
+import softeer.h9.hey.domain.car.InteriorColor;
 
 @Repository
 @RequiredArgsConstructor
@@ -18,8 +19,9 @@ public class InteriorColorRepository {
     private final NamedParameterJdbcTemplate jdbcTemplate;
 
     public List<InteriorColor> findAllByTrimId(final int trimId) {
-        String sql = "SELECT DISTINCT i.id AS id, name, color_image_url, interior_color_id AS car_image_url, additional_price, ei.trim_id " +
-                "FROM interiorColor i,exteriorColor_interiorColor ei " +
+        String sql = "SELECT DISTINCT i.id AS id, name, color_image_url, interior_color_id AS car_image_url, additional_price, ei.trim_id "
+            +
+            "FROM interiorColor i,exteriorColor_interiorColor ei " +
                 "WHERE ei.trim_id = :trim_id AND ei.interior_color_id = i.id " +
                 "ORDER BY i.id";
         SqlParameterSource param = new MapSqlParameterSource()
