4fc444214e91b8b54f3ef32ffe11c5e44e614874
ddingmin <ddingminyt@gmail.com>
2023-08-12 14:18:03 +0900

    #72 feat: 조합 색상 Interger Type으로 변경

--- backend/src/main/java/softeer/h9/hey/repository/car/AvailableColorRepository.java
+++ backend/src/main/java/softeer/h9/hey/repository/car/AvailableColorRepository.java
@@ -1,31 +1,25 @@
 package softeer.h9.hey.repository.car;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.RowMapper;
+import java.util.List;
+
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
-import softeer.h9.hey.domain.car.AvailableColor;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
 public class AvailableColorRepository {
 
-    private final NamedParameterJdbcTemplate jdbcTemplate;
+	private final NamedParameterJdbcTemplate jdbcTemplate;
 
-    public List<AvailableColor> findAllByTrimId(final int trimId) {
-        String sql = "SELECT * FROM exteriorColor_interiorColor WHERE trim_id = :trim_id";
-        SqlParameterSource param = new MapSqlParameterSource()
-                .addValue("trim_id", trimId);
+	public List<Integer> findAllByTrimIdAndExteriorColor(final int trimId, final int exteriorColorId) {
+		String sql = "SELECT interior_color_id FROM exteriorColor_interiorColor WHERE trim_id = :trim_id AND exterior_color_id = :exterior_color_id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("trim_id", trimId)
+			.addValue("exterior_color_id", exteriorColorId);
 
-        return jdbcTemplate.query(sql, param, availableColorRowMapper());
-    }
-
-    private RowMapper<AvailableColor> availableColorRowMapper() {
-        return BeanPropertyRowMapper.newInstance(AvailableColor.class);
-    }
+		return jdbcTemplate.queryForList(sql, param, Integer.class);
+	}
 }
