dacc4070a7ed45020f0967ab3bcd30c3ebfc8e04
lee-yun-pyo <yun2193@hanmail.net>
2023-08-12 15:02:19 +0900

    #73 feat: 마이카이빙 화살표 버튼 기능 추가

--- frontend/src/pages/MyArchiving/index.tsx
+++ frontend/src/pages/MyArchiving/index.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
+
 import { MyCarList } from '@/components/archiving/MyCarList';
 import { ArchivingHeader } from '@/components/common/ArchivingHeader';
 import { ArchivingNavigation } from '@/components/common/ArchivingNavigation';
+import { PrevButton } from '@/components/common/PrevButton';
+import { NextButton } from '@/components/common/NextButton';
 
 import * as style from './style';
 
@@ -8,9 +12,10 @@ const archivingMockData = {
   data: {
     myarchivings: [
       {
-        id: '1',
+        id: 1,
         model: '팰리세이드',
         trim: 'Le Blanc',
+        isSaved: true,
         trimOptions: ['디젤 2.2', '4WD', '7인승'],
         lastModifiedDate: '2023-07-19',
         selectedOptions: [
@@ -25,9 +30,28 @@ const archivingMockData = {
         ],
       },
       {
-        id: '2',
+        id: 2,
         model: '팰리세이드',
         trim: 'Le Blanc',
+        isSaved: true,
+        trimOptions: ['디젤 2.2', '4WD', '7인승'],
+        lastModifiedDate: '2023-07-19',
+        selectedOptions: [
+          {
+            name: '컴포트 ||',
+            imageUrl: 'https://www.hyundai.com/contents/vr360/LX06/exterior/A2B/colorchip-exterior.png',
+          },
+          {
+            name: '듀얼 와이드 선루프',
+            imageUrl: 'https://www.hyundai.com/contents/vr360/LX06/exterior/A2B/colorchip-exterior.png',
+          },
+        ],
+      },
+      {
+        id: 3,
+        model: '팰리세이드',
+        trim: 'Le Blanc',
+        isSaved: true,
         trimOptions: ['디젤 2.2', '2WD', '7인승'],
         lastModifiedDate: '2023-07-15',
         selectedOptions: [
@@ -61,9 +85,10 @@ const tempArchivingMockData = {
   data: {
     myarchivings: [
       {
-        id: '1',
+        id: 13,
         model: '팰리세이드',
         trim: 'Le Blanc',
+        isSaved: false,
         trimOptions: ['디젤 2.2', '4WD', '7인승'],
         lastModifiedDate: '2023-07-19',
         selectedOptions: [
@@ -78,20 +103,107 @@ const tempArchivingMockData = {
         ],
       },
       {
-        id: '2',
+        id: 14,
         model: '팰리세이드',
         trim: 'Le Blanc',
-        trimOptions: ['디젤 2.2', '2WD', '7인승'],
-        lastModifiedDate: '2023-07-15',
-        selectedOptions: [],
+        isSaved: false,
+        trimOptions: ['디젤 2.2', '4WD', '7인승'],
+        lastModifiedDate: '2023-07-19',
+        selectedOptions: [
+          {
+            name: '컴포트 ||',
+            imageUrl: 'https://www.hyundai.com/contents/vr360/LX06/exterior/P7V/002.png',
+          },
+          {
+            name: '듀얼 와이드 선루프',
+            imageUrl: 'https://www.hyundai.com/contents/vr360/LX06/exterior/P7V/002.png',
+          },
+        ],
+      },
+      {
+        id: 15,
+        model: '팰리세이드',
+        trim: 'Le Blanc',
+        isSaved: false,
+        trimOptions: ['디젤 2.2', '4WD', '7인승'],
+        lastModifiedDate: '2023-07-19',
+        selectedOptions: [
+          {
+            name: '컴포트 ||',
+            imageUrl: 'https://www.hyundai.com/contents/vr360/LX06/exterior/P7V/002.png',
+          },
+          {
+            name: '듀얼 와이드 선루프',
+            imageUrl: 'https://www.hyundai.com/contents/vr360/LX06/exterior/P7V/002.png',
+          },
+        ],
+      },
+      {
+        id: 16,
+        model: '팰리세이드',
+        trim: 'Le Blanc',
+        isSaved: false,
+        trimOptions: ['디젤 2.2', '4WD', '7인승'],
+        lastModifiedDate: '2023-07-19',
+        selectedOptions: [
+          {
+            name: '컴포트 ||',
+            imageUrl: 'https://www.hyundai.com/contents/vr360/LX06/exterior/P7V/002.png',
+          },
+          {
+            name: '듀얼 와이드 선루프',
+            imageUrl: 'https://www.hyundai.com/contents/vr360/LX06/exterior/P7V/002.png',
+          },
+        ],
+      },
+      {
+        id: 17,
+        model: '팰리세이드',
+        trim: 'Le Blanc',
+        isSaved: false,
+        trimOptions: ['디젤 2.2', '4WD', '7인승'],
+        lastModifiedDate: '2023-07-19',
+        selectedOptions: [
+          {
+            name: '컴포트 ||',
+            imageUrl: 'https://www.hyundai.com/contents/vr360/LX06/exterior/P7V/002.png',
+          },
+          {
+            name: '듀얼 와이드 선루프',
+            imageUrl: 'https://www.hyundai.com/contents/vr360/LX06/exterior/P7V/002.png',
+          },
+        ],
+      },
+      {
+        id: 18,
+        model: '팰리세이드',
+        trim: 'Le Blanc',
+        isSaved: false,
+        trimOptions: ['디젤 2.2', '4WD', '7인승'],
+        lastModifiedDate: '2023-07-19',
+        selectedOptions: [
+          {
+            name: '컴포트 ||',
+            imageUrl: 'https://www.hyundai.com/contents/vr360/LX06/exterior/P7V/002.png',
+          },
+          {
+            name: '듀얼 와이드 선루프',
+            imageUrl: 'https://www.hyundai.com/contents/vr360/LX06/exterior/P7V/002.png',
+          },
+        ],
       },
     ],
   },
 };
 
 export function MyArchiving() {
-  const data = archivingMockData.data.myarchivings;
   const tempData = tempArchivingMockData.data.myarchivings;
+  const data = archivingMockData.data.myarchivings;
+  const allData = [...tempData, ...data];
+
+  const [page, setPage] = useState(0);
+  const rangeArray = Array.from({ length: 4 }, (_, index) => index + page * 4);
+
   return (
     <>
       <ArchivingHeader />
@@ -100,30 +212,41 @@ export function MyArchiving() {
         <style.Wrapper>
           <style.TitleText>내가 만든 차량 목록</style.TitleText>
           <style.Division />
-          <style.MyCarBox>
-            {tempData.map(data => (
-              <MyCarList
-                key={data.id}
-                isSaved={false}
-                model={data.model}
-                trim={data.trim}
-                trimOptions={data.trimOptions}
-                lastModifiedDate={data.lastModifiedDate}
-                selectedOptions={data.selectedOptions}
-              />
-            ))}
-            {data.map(data => (
-              <MyCarList
-                key={data.id}
-                isSaved={true}
-                model={data.model}
-                trim={data.trim}
-                trimOptions={data.trimOptions}
-                lastModifiedDate={data.lastModifiedDate}
-                selectedOptions={data.selectedOptions}
-              />
-            ))}
-          </style.MyCarBox>
+          <style.MyCarMain>
+            <PrevButton
+              width="60"
+              height="60"
+              onClick={() => {
+                setPage(prev => prev - 1);
+              }}
+              isShow={page !== 0}
+            />
+            <style.MyCarBox>
+              {rangeArray.map(index =>
+                index < allData.length ? (
+                  <MyCarList
+                    key={allData[index].id}
+                    isSaved={allData[index].isSaved}
+                    model={allData[index].model}
+                    trim={allData[index].trim}
+                    trimOptions={allData[index].trimOptions}
+                    lastModifiedDate={allData[index].lastModifiedDate}
+                    selectedOptions={allData[index].selectedOptions}
+                  />
+                ) : (
+                  <style.EmptyBox key={index}></style.EmptyBox>
+                )
+              )}
+            </style.MyCarBox>
+            <NextButton
+              width="60"
+              height="60"
+              onClick={() => {
+                setPage(prev => prev + 1);
+              }}
+              isShow={page !== Math.floor(allData.length < 1 ? 0 : Math.floor((allData.length - 1) / 4))}
+            />
+          </style.MyCarMain>
         </style.Wrapper>
       </style.Container>
     </>
--- frontend/src/pages/MyArchiving/style.tsx
+++ frontend/src/pages/MyArchiving/style.tsx
@@ -37,9 +37,22 @@ export const Division = styled.div`
   }}
 `;
 
+export const MyCarMain = styled.div`
+  gap: 15px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const MyCarBox = styled.div`
   gap: 20px;
 
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+`;
+
+export const EmptyBox = styled.div`
+  width: 506px;
+  height: 239px;
 `;
