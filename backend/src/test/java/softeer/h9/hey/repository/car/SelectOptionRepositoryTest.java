5732f2d203c3d35e20283c5fa30007377b857b74
yun <yshyeon102@gmail.com>
2023-08-13 17:59:01 +0900

    #87 test: 선택 옵션, N performance 옵션 DB 조회 기능 테스트 추가

---
+++ backend/src/test/java/softeer/h9/hey/repository/car/SelectOptionRepositoryTest.java
@@ -0,0 +1,47 @@
+package softeer.h9.hey.repository.car;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import softeer.h9.hey.domain.car.Category;
+import softeer.h9.hey.domain.car.SelectOption;
+
+@SpringBootTest
+@DisplayName("선택 옵션 저장소 테스트")
+public class SelectOptionRepositoryTest {
+
+	@Autowired
+	SelectOptionRepository repository;
+
+	@DisplayName("특정 자동차에 적용할 수 있는 선택 옵션을 조회한다.")
+	@Test
+	void findByCarCodeTest() {
+		String carCode = "LXJJ8MST5";
+
+		List<SelectOption> selectOptions = repository.findSelectOptionsByCarCode(carCode);
+
+		assertThat(selectOptions).hasSize(6);
+		for (SelectOption selectOption : selectOptions) {
+			assertThat(selectOption.getCategory()).isEqualTo(Category.SELECT_OPTION);
+		}
+	}
+
+	@DisplayName("특정 자동차에 적용할 수 있는 N Performance 옵션을 조회한다.")
+	@Test
+	void findNPerformanceOptionTest() {
+		String carCode = "LXJJ8MST5";
+
+		List<SelectOption> selectOptions = repository.findNPerformanceByCarCode(carCode);
+
+		assertThat(selectOptions).hasSize(3);
+		for (SelectOption selectOption : selectOptions) {
+			assertThat(selectOption.getCategory()).isEqualTo(Category.N_PERFORMANCE);
+		}
+	}
+}
