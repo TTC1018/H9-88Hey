b7c516ab927ee83a5f133dfa46f5e69105713c79
RecordOfJun <pritty0731@naver.com>
2023-08-14 02:37:16 +0900

    #88 test: car code를 통해 선택할 수 있는 H Genuine Accessory 목록을 조회하는 기능 테스트 작성

--- backend/src/test/java/softeer/h9/hey/controller/car/SelectOptionControllerTest.java
+++ backend/src/test/java/softeer/h9/hey/controller/car/SelectOptionControllerTest.java
@@ -56,4 +56,24 @@ class SelectOptionControllerTest {
 				jsonPath("$.data.selectOptions[0].subOptions[0].description").exists()
 			);
 	}
+
+	@Test
+	@DisplayName("car code를 통해 선택할 수 있는 H Genuine Accessory 목록을 조회한다")
+	void getAvailableHGenuineOptionTest() throws Exception {
+		mockMvc.perform(
+				get("/car/h-genuine-accessories")
+					.param("carCode", "LXJJ7DCT5")
+					.param("seletOption", "VI2"))
+			.andExpect(status().isOk())
+			.andExpectAll(
+				jsonPath("$.data.selectOptions").exists(),
+				jsonPath("$.data.selectOptions[0].isAvailable").exists(),
+				jsonPath("$.data.selectOptions[0].selectOption").exists(),
+				jsonPath("$.data.selectOptions[0].selectOption.subOptions[0]").exists(),
+				jsonPath("$.data.selectOptions[0].selectOption.subOptions[0].id").exists(),
+				jsonPath("$.data.selectOptions[0].selectOption.subOptions[0].name").exists(),
+				jsonPath("$.data.selectOptions[0].selectOption.subOptions[0].imageURL").exists(),
+				jsonPath("$.data.selectOptions[0].selectOption.subOptions[0].description").exists()
+			);
+	}
 }
