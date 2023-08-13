2c560e3428f1370858edf2c50f7265928a279aad
DongHyeon Park <paramore@kakao.com>
2023-08-13 21:29:27 +0900

    #92 feat: Retrofit 모듈 선언

---
+++ android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/di/NetworkModule.kt
@@ -0,0 +1,36 @@
+package com.softeer.mycarchiving.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "http://ec2-3-38-162-121.ap-northeast-2.compute.amazonaws.com:8080/"
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .readTimeout(5000, TimeUnit.MILLISECONDS)
+            .connectTimeout(5000, TimeUnit.MILLISECONDS)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+}
