e65c79c00ba8a1827e55ba5a502253ef5c06b8d6
yun <yshyeon102@gmail.com>
2023-08-13 18:17:03 +0900

    #87 test: 선택 옵션, 하위 옵션 조회 요청 및 응답 데이터 검증 테스트 추가

---
+++ backend/src/test/java/softeer/h9/hey/controller/car/SelectOptionControllerTest.java
@@ -0,0 +1,59 @@
+package softeer.h9.hey.controller.car;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SelectOptionControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@Test
+	@DisplayName("car code를 통해 선택할 수 있는 선택 옵션 목록을 조회한다.")
+	void getSelectOptionTest() throws Exception {
+		mockMvc.perform(
+				get("/car/select-option")
+					.param("carCode", "LXJJ8MST5"))
+			.andExpect(status().isOk())
+			.andExpectAll(
+				jsonPath("$.data.selectOptions").exists(),
+				jsonPath("$.data.selectOptions[0].id").exists(),
+				jsonPath("$.data.selectOptions[0].name").exists(),
+				jsonPath("$.data.selectOptions[0].imageURL").exists(),
+				jsonPath("$.data.selectOptions[0].subOptions[0]").exists(),
+				jsonPath("$.data.selectOptions[0].subOptions[0].id").exists(),
+				jsonPath("$.data.selectOptions[0].subOptions[0].name").exists(),
+				jsonPath("$.data.selectOptions[0].subOptions[0].imageURL").exists(),
+				jsonPath("$.data.selectOptions[0].subOptions[0].description").exists()
+			);
+	}
+
+	@Test
+	@DisplayName("car code를 통해 선택할 수 있는 N Performance 목록을 조회한다.")
+	void getNPerformanceOptionTest() throws Exception {
+		mockMvc.perform(
+				get("/car/n-performance")
+					.param("carCode", "LXJJ8MST5"))
+			.andExpect(status().isOk())
+			.andExpectAll(
+				jsonPath("$.data.selectOptions").exists(),
+				jsonPath("$.data.selectOptions[0].id").exists(),
+				jsonPath("$.data.selectOptions[0].name").exists(),
+				jsonPath("$.data.selectOptions[0].imageURL").exists(),
+				jsonPath("$.data.selectOptions[0].subOptions[0]").exists(),
+				jsonPath("$.data.selectOptions[0].subOptions[0].id").exists(),
+				jsonPath("$.data.selectOptions[0].subOptions[0].name").exists(),
+				jsonPath("$.data.selectOptions[0].subOptions[0].imageURL").exists(),
+				jsonPath("$.data.selectOptions[0].subOptions[0].description").exists()
+			);
+	}
+}
