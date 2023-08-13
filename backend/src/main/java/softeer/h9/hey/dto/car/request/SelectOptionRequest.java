87a038510b3eb88cc232e59dedec407ec7ab4561
RecordOfJun <pritty0731@naver.com>
2023-08-13 19:26:03 +0900

    #88 feat: SelectOptionRequest 차량 코드 필수값 설정, 선택 옵션 배열 파라미터 추가

--- backend/src/main/java/softeer/h9/hey/dto/car/request/SelectOptionRequest.java
+++ backend/src/main/java/softeer/h9/hey/dto/car/request/SelectOptionRequest.java
@@ -1,5 +1,9 @@
 package softeer.h9.hey.dto.car.request;
 
+import java.util.List;
+
+import javax.validation.constraints.NotEmpty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,5 +14,8 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SelectOptionRequest {
+	@NotEmpty
 	private String carCode;
+
+	private List<String> selectedOptionCodes;
 }
