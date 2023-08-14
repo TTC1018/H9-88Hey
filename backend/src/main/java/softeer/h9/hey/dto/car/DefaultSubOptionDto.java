606faff1e3bf9a89d7883c33ddaf6130f961a910
ddingmin <ddingminyt@gmail.com>
2023-08-14 16:16:54 +0900

    #93 feat: 필요한 값들만 사용하도록 수정

--- backend/src/main/java/softeer/h9/hey/dto/car/DefaultSubOptionDto.java
+++ backend/src/main/java/softeer/h9/hey/dto/car/DefaultSubOptionDto.java
@@ -5,19 +5,17 @@ import lombok.Getter;
 @Getter
 public class DefaultSubOptionDto {
 
-	private final int id;
 	private final String name;
 	private final String imageUrl;
 	private final String description;
 
-	private DefaultSubOptionDto(int id, String name, String imageUrl, String description) {
-		this.id = id;
+	private DefaultSubOptionDto(String name, String imageUrl, String description) {
 		this.name = name;
 		this.imageUrl = imageUrl;
 		this.description = description;
 	}
 
-	public static DefaultSubOptionDto of(int id, String name, String imageUrl, String description) {
-		return new DefaultSubOptionDto(id, name, imageUrl, description);
+	public static DefaultSubOptionDto of(String name, String imageUrl, String description) {
+		return new DefaultSubOptionDto(name, imageUrl, description);
 	}
 }
