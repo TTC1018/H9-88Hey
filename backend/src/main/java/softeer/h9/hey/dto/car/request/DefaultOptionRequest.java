823324de3d7c1df17d79395dfce1502f9c2cdf23
ddingmin <ddingminyt@gmail.com>
2023-08-14 09:18:42 +0900

    #93 feat: 자동차 코드 요청 DTO 구현

---
+++ backend/src/main/java/softeer/h9/hey/dto/car/request/DefaultOptionRequest.java
@@ -0,0 +1,12 @@
+package softeer.h9.hey.dto.car.request;
+
+import lombok.Getter;
+
+@Getter
+public class DefaultOptionRequest {
+	private final String carCode;
+
+	private DefaultOptionRequest(String car_code) {
+		this.carCode = car_code;
+	}
+}
