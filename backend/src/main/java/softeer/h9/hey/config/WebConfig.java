400d6144c94d2fb8a15e31ac0e16e13d1806f93f
윤상현 <yshyeon102@gmail.com>
2023-08-15 15:52:40 +0900

    #119 feat: CORS 허용 기능 구현

---
+++ backend/src/main/java/softeer/h9/hey/config/WebConfig.java
@@ -0,0 +1,21 @@
+package softeer.h9.hey.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	public static final int PRE_FLIGHT_CACHE_SECOND = 3600;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOriginPatterns("*") // 수정 필요!
+			.allowCredentials(true)
+			.allowedMethods("*")
+			.allowedHeaders("*")
+			.maxAge(PRE_FLIGHT_CACHE_SECOND);
+	}
+}
