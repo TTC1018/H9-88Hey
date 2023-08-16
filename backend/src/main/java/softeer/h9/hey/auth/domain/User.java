c17da78d6f46b02d402e220525218227b75ba2ca
윤상현 <yshyeon102@gmail.com>
2023-08-17 03:04:05 +0900

    #129 feat: User 도메인 클래스 정의

---
+++ backend/src/main/java/softeer/h9/hey/auth/domain/User.java
@@ -0,0 +1,14 @@
+package softeer.h9.hey.auth.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class User {
+	private final Integer id;
+	private final String userId;
+	private final String password;
+}
