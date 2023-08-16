04e6f48a656fc20f55415c791d58e0f741f23ea4
RecordOfJun <pritty0731@naver.com>
2023-08-16 10:52:28 +0900

    #130 fix: saleModelCodes 수정

--- util/palisade_option_extraction/default_option_mapping.py
+++ util/palisade_option_extraction/default_option_mapping.py
@@ -5,11 +5,12 @@ from collections import defaultdict
 saleModelCodes = ["LXJJ7DST5", "LXJJ7DAT5", "LXJJ7DBT5", "LXJJ7DCT5",
                   "LXJJ7MST5", "LXJJ7MAT5", "LXJJ7MBT5", "LXJJ7MCT5",
                   "LXJJ8DST5", "LXJJ8DAT5", "LXJJ8DBT5", "LXJJ8DCT5",
-                  "LXJJ8MST5", "LXJJ7MAT5", "LXJJ7MBT5", "LXJJ7MCT5",
+                  "LXJJ8MST5", "LXJJ8MAT5", "LXJJ8MBT5", "LXJJ8MCT5",
                   "LXJJ7DSA5", "LXJJ7DAA5", "LXJJ7DBA5", "LXJJ7DCA5",
                   "LXJJ8DSA5", "LXJJ8DAA5", "LXJJ8DBA5", "LXJJ8DCA5",
                   "LXJJ8MSA5", "LXJJ8MAA5", "LXJJ8MBA5", "LXJJ8MCA5",
                   "LXJJ7MSA5", "LXJJ7MAA5", "LXJJ7MBA5", "LXJJ7MCA5"]
+
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -33,7 +34,7 @@ def mapping_default_option_with_model(saleModelCode):
         #옵션 삽입
         for option in car_options:
             option_id = option['basicOptionCode']
-            mycursor.execute("INSERT INTO defaultoption_carTrims (model_id,default_option_id) VALUES (%s,%s)"
+            mycursor.execute("INSERT INTO defaultOption_carTrims (car_normal_types_id,default_option_id) VALUES (%s,%s)"
                        %(paramify(saleModelCode),paramify(option_id)))
             mydb.commit()
 
