3e0b9e22b7164ccd277a17ef0a77a9131c092e5c
RecordOfJun <pritty0731@naver.com>
2023-08-17 15:33:34 +0900

    #128 feat: 임시저장 요청을 받기 위한 MyChivingTempSaveRequest.java 구현

---
+++ backend/src/main/java/softeer/h9/hey/dto/car/request/MyChivingTempSaveRequest.java
@@ -0,0 +1,30 @@
+package softeer.h9.hey.dto.car.request;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class MyChivingTempSaveRequest {
+	@JsonProperty("id")
+	private final Long id;
+	@JsonProperty("bodyType")
+	private final Integer bodyTypeId;
+	@JsonProperty("wheelType")
+	private final Integer wheelTypeId;
+	@JsonProperty("engine")
+	private final Integer engineId;
+	@JsonProperty("trim")
+	private final Integer trimId;
+	@JsonProperty("exteriorColor")
+	private final Integer exteriorColorId;
+	@JsonProperty("interiorColor")
+	private final Integer interiorColorId;
+	@JsonProperty("selectOptions")
+	private final List<String> selectOptionIdList;
+
+}
