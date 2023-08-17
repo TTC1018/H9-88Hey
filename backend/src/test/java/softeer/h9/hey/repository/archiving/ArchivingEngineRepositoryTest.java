f099ab8e77f4c3bb7738b93ef786dba994077407
ddingmin <ddingminyt@gmail.com>
2023-08-18 01:39:44 +0900

    #137 test: 바디타입 repository 테스트 작성

--- backend/src/test/java/softeer/h9/hey/repository/archiving/ArchivingEngineRepositoryTest.java
+++ backend/src/test/java/softeer/h9/hey/repository/archiving/ArchivingEngineRepositoryTest.java
@@ -7,25 +7,25 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import softeer.h9.hey.dto.archiving.BodyTypeDto;
+import softeer.h9.hey.dto.archiving.EngineDto;
 
 @SpringBootTest
 @DisplayName("아카이빙 BodyType Repository 테스트")
-class ArchivingBodyTypeRepositoryTest {
+class ArchivingEngineRepositoryTest {
 
 	@Autowired
-	private ArchivingBodyTypeRepository repository;
+	private ArchivingEngineRepository repository;
 
 	@DisplayName("아카이빙 car code기반으로 BodyType을 조회한다.")
 	@Test
 	void findByCarCode() {
 		String carCode = "LXJJ7MCT5";
 
-		BodyTypeDto dto = repository.findByCarCode(carCode);
+		EngineDto dto = repository.findByCarCode(carCode);
 
 		assertAll(
 			() -> assertEquals(1, dto.getId()),
-			() -> assertEquals("7인승", dto.getName()),
+			() -> assertEquals("디젤 2.2", dto.getName()),
 			() -> assertEquals(0, dto.getAdditionalPrice())
 		);
 	}
