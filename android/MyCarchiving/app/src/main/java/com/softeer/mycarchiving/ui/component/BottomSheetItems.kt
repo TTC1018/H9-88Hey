b86f1e915f66b4898ecb3a2fc609d7583a1586bc
DongHyeon Park <paramore@kakao.com>
2023-08-17 11:21:36 +0900

    #101 fix: Rebase 누락 코드 추가
    Co-Authored-By: 안승우 <78468001+SeungWoo-Ahn@users.noreply.github.com>

--- android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/ui/component/BottomSheetItems.kt
+++ android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/ui/component/BottomSheetItems.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
