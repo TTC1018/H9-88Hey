3965372cf7bed7aec4ad4f455b8fb129f75b92c4
윤상현 <yshyeon102@gmail.com>
2023-08-17 16:49:51 +0900

    #129 test: 테스트 코드 오타 수정 및 등록되지 않은 회원이 로그인하는 경우 예외 발생 테스트 추가

--- backend/src/test/java/softeer/h9/hey/auth/service/AuthServiceTest.java
+++ backend/src/test/java/softeer/h9/hey/auth/service/AuthServiceTest.java
@@ -4,12 +4,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import softeer.h9.hey.auth.dto.request.JoinRequest;
+import softeer.h9.hey.auth.dto.request.LoginRequest;
+import softeer.h9.hey.auth.dto.response.TokenResponse;
 import softeer.h9.hey.auth.exception.JoinException;
 import softeer.h9.hey.auth.exception.LoginException;
 
 @DisplayName("인증 관련 서비스 기능 테스트")
 class AuthServiceTest {
-
 	private final AuthService authService = new AuthService();
 
 	@Test
@@ -19,8 +21,8 @@ class AuthServiceTest {
 
 		TokenResponse tokenResponse = authService.join(joinRequest);
 
-		Assertions.assertThat(tokenResponse.getAccessToken).isNotNull();
-		Assertions.assertThat(tokenResponse.getRefreshToken).isNotNull();
+		Assertions.assertThat(tokenResponse.getAccessToken()).isNotNull();
+		Assertions.assertThat(tokenResponse.getRefreshToken()).isNotNull();
 	}
 
 	@Test
@@ -37,23 +39,32 @@ class AuthServiceTest {
 	@Test
 	@DisplayName("로그인 테스트")
 	void loginTest() {
-		JoinRequest joinRequest1 = new JoinRequest("userId1", "password1");
-		authService.join(joinRequest1);
+		JoinRequest joinRequest = new JoinRequest("userId1", "password1");
+		authService.join(joinRequest);
 
 		LoginRequest loginRequest = new LoginRequest("userId1", "password1");
-		TokenResponse tokenResponse = authService.login(joinRequest1);
+		TokenResponse tokenResponse = authService.login(loginRequest);
 
-		Assertions.assertThat(tokenResponse.getAccessToken).isNotNull();
-		Assertions.assertThat(tokenResponse.getRefreshToken).isNotNull();
+		Assertions.assertThat(tokenResponse.getAccessToken()).isNotNull();
+		Assertions.assertThat(tokenResponse.getRefreshToken()).isNotNull();
 	}
 
 	@Test
-	@DisplayName("로그인 실패 테스트")
-	void loginFailTest() {
-		JoinRequest joinRequest1 = new JoinRequest("userId1", "password1");
-		authService.join(joinRequest1);
+	@DisplayName("비밀번호가 틀린 경우 로그인 실패 테스트")
+	void loginFailTest1() {
+		JoinRequest joinRequest = new JoinRequest("userId1", "password1");
+		authService.join(joinRequest);
 
 		LoginRequest loginRequest = new LoginRequest("userId1", "password2");
+
+		Assertions.assertThatThrownBy(() -> authService.login(loginRequest))
+			.isInstanceOf(LoginException.class);
+	}
+
+	@Test
+	@DisplayName("요청 ID로 등록된 회원이 없는 경우 로그인 실패 테스트")
+	void loginFailTest2() {
+		LoginRequest loginRequest = new LoginRequest("userId", "password");
 
 		Assertions.assertThatThrownBy(() -> authService.login(loginRequest))
 			.isInstanceOf(LoginException.class);
