6e9ce1f010e7301b70f899857f238936dbadcbea
ddingmin <ddingminyt@gmail.com>
2023-08-17 22:47:46 +0900

    #137 feat: 아카이빙 bodyType dto 수정

--- backend/src/main/java/softeer/h9/hey/dto/archiving/BodyTypeDto.java
+++ backend/src/main/java/softeer/h9/hey/dto/archiving/BodyTypeDto.java
@@ -1,17 +1,21 @@
 package softeer.h9.hey.dto.archiving;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class BodyTypeDto {
-	private final Integer id;
-	private final String name;
-	private final String additionalPrice;
+	private Integer id;
+	private String name;
+	private Integer additionalPrice;
 
-	public static BodyTypeDto of(final Integer id, final String name, final String additionalPrice) {
+	public static BodyTypeDto of(Integer id, String name, Integer additionalPrice) {
 		return new BodyTypeDto(id, name, additionalPrice);
 	}
 }
