0d5577248f9be14fe5b7b927ecc7662bd3b164c4
윤상현 <yshyeon102@gmail.com>
2023-08-17 11:46:00 +0900

    #129 test:  User를 저장, 조회 테스트 추가

--- backend/src/test/java/softeer/h9/hey/auth/repository/UserRepositoryTest.java
+++ backend/src/test/java/softeer/h9/hey/auth/repository/UserRepositoryTest.java
@@ -8,10 +8,12 @@ import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import softeer.h9.hey.auth.domain.User;
 
+@SpringBootTest
 @Transactional
 @DisplayName("User 저장소 테스트")
 class UserRepositoryTest {
