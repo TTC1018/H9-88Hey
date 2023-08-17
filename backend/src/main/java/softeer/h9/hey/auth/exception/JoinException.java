c160ec0809eac6a26762fe8fd8510fc61eb6b707
윤상현 <yshyeon102@gmail.com>
2023-08-18 01:35:28 +0900

    #129 fix: JoinException 예외 메세지 수정

--- backend/src/main/java/softeer/h9/hey/auth/exception/JoinException.java
+++ backend/src/main/java/softeer/h9/hey/auth/exception/JoinException.java
@@ -2,7 +2,7 @@ package softeer.h9.hey.auth.exception;
 
 public class JoinException extends IllegalArgumentException {
 
-	public static final String LOGIN_FAIL_MESSAGE = "로그인 실패";
+	public static final String LOGIN_FAIL_MESSAGE = "회원가입 실패";
 
 	public JoinException() {
 		super(LOGIN_FAIL_MESSAGE);
