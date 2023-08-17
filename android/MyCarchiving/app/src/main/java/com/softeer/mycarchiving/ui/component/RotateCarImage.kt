ad58d02dbf4fa9913ce67b79ea7c9375468722de
DongHyeon Park <paramore@kakao.com>
2023-08-18 02:28:10 +0900

    #144 feat: 색상선택 진입 전 이미지 캐싱

--- android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/ui/component/RotateCarImage.kt
+++ android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/ui/component/RotateCarImage.kt
@@ -1,21 +1,28 @@
 package com.softeer.mycarchiving.ui.component
 
 import android.util.Log
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,9 +30,12 @@ import coil.ImageLoader
 import coil.compose.AsyncImage
 import coil.memory.MemoryCache
 import coil.request.ImageRequest
+import com.softeer.mycarchiving.ui.theme.PrimaryBlue
+import com.softeer.mycarchiving.util.fadeInAndOut
 import kotlinx.coroutines.launch
 
 private val TAG = "RotateCarImage"
+private const val IMAGE_360_SIZE = 60
 
 @Composable
 fun RotateCarImage(
@@ -33,7 +43,7 @@ fun RotateCarImage(
     imagePath: String,
     selectedIndex: Int,
     onLeftClick: () -> Unit,
-    onRightClick: () -> Unit
+    onRightClick: () -> Unit,
 ) {
     val context = LocalContext.current
     val interactionSource = remember { MutableInteractionSource() }
@@ -41,33 +51,38 @@ fun RotateCarImage(
         ImageLoader.Builder(context)
             .memoryCache {
                 MemoryCache.Builder(context)
-                    .maxSizePercent(0.4)
+                    .maxSizePercent(1.0)
                     .build()
-            }.build()
+            }
+            .build()
     }
+    var loadCounter by remember { mutableIntStateOf(0) }
+    val loadEnded by remember(loadCounter) { derivedStateOf { loadCounter != 0 && loadCounter == IMAGE_360_SIZE } }
 
-    // image preload
     LaunchedEffect(imagePath) {
-        // 이전 이미지 29개
+        loadCounter = 0
+        // 다음 이미지 30개
         launch {
-            for (imageNum in selectedIndex + 1 until selectedIndex + 30) {
+            for (imageNum in selectedIndex until selectedIndex + IMAGE_360_SIZE.div(2)) {
                 launch {
                     imageLoader.execute(
                         ImageRequest.Builder(context)
-                            .data(imagePath.imagePathToUrl((imageNum.mod(60)) + 1))
+                            .data(imagePath.imagePathToUrl((imageNum.mod(IMAGE_360_SIZE)) + 1))
+                            .listener { _, _ -> loadCounter++ }
                             .build()
                     )
                 }
             }
         }
 
-        // 다음 이미지 30개
+        // 이전 이미지 30개
         launch {
-            for (imageNum in selectedIndex - 1 downTo  selectedIndex - 30) {
+            for (imageNum in selectedIndex - 1 downTo selectedIndex - IMAGE_360_SIZE.div(2)) {
                 launch {
                     imageLoader.execute(
                         ImageRequest.Builder(context)
-                            .data(imagePath.imagePathToUrl((imageNum.mod(60)) + 1))
+                            .data(imagePath.imagePathToUrl((imageNum.mod(IMAGE_360_SIZE)) + 1))
+                            .listener { _, _ -> loadCounter++ }
                             .build()
                     )
                 }
@@ -78,15 +93,40 @@ fun RotateCarImage(
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .height(200.dp)
+            .height(200.dp),
+        contentAlignment = Alignment.Center
     ) {
-        AsyncImage(
-            modifier = Modifier
-                .fillMaxSize(),
-            model = imagePath.imagePathToUrl(selectedIndex + 1),
-            contentDescription = "",
-            imageLoader = imageLoader
-        )
+        AnimatedContent(
+            targetState = loadEnded,
+            transitionSpec = { fadeInAndOut() },
+            contentAlignment = Alignment.Center,
+            label = ""
+        ) {
+            when (it) {
+                true -> {
+                    AsyncImage(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        model = imagePath.imagePathToUrl(selectedIndex + 1),
+                        contentDescription = "",
+                        imageLoader = imageLoader
+                    )
+                }
+
+                false -> {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .fillMaxHeight(0.5f)
+                            .aspectRatio(1f),
+                        progress = loadCounter.toFloat() / IMAGE_360_SIZE,
+                        color = PrimaryBlue,
+                        trackColor = Color.Transparent,
+                        strokeWidth = 10.dp
+                    )
+                }
+            }
+        }
+
         Row(
             modifier = Modifier.fillMaxSize()
         ) {
