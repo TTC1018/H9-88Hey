a1390345ecb46ecada9cafababe3c4c8a3a5a3e0
SeungWoo-Ahn <rio970319@gmail.com>
2023-08-18 17:23:51 +0900

    #124 feat: 마이카이빙 중첩 NavHost 구현

---
+++ android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/ui/myarchive/myarchivemade/MyArchiveMadeNavigation.kt
@@ -0,0 +1,26 @@
+package com.softeer.mycarchiving.ui.myarchive.myarchivemade
+
+import androidx.activity.compose.BackHandler
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.softeer.mycarchiving.navigation.MyArchiveDestinations
+
+fun NavController.navigateToMyArchiveMade(navOptions: NavOptions? = null) {
+    navigate(MyArchiveDestinations.MY_ARCHIVE_MADE.route, navOptions)
+}
+
+fun NavGraphBuilder.myArchiveMadeScreen(
+    moveDetailPage: () -> Unit,
+    onBackClick: () -> Unit,
+) {
+    composable(
+        route = MyArchiveDestinations.MY_ARCHIVE_MADE.route
+    ) {
+        BackHandler {
+            onBackClick()
+        }
+        MyArchiveMadeRoute()
+    }
+}
