a6edf4d088f772afcf733f4c52ea5f755634c6aa
윤상현 <yshyeon102@gmail.com>
2023-08-19 13:52:46 +0900

    #129 feat: RefreshToken DAO 저장, 조회 기능 구현

---
+++ backend/src/main/java/softeer/h9/hey/auth/repository/RefreshTokenRepositoryImpl.java
@@ -0,0 +1,44 @@
+package softeer.h9.hey.auth.repository;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+import softeer.h9.hey.auth.domain.RefreshTokenEntity;
+
+@Repository
+@RequiredArgsConstructor
+public class RefreshTokenRepositoryImpl implements RefreshTokenRepository {
+
+	private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+	@Override
+	public void save(RefreshTokenEntity refreshTokenEntity) {
+		String sql = "INSERT INTO `refreshToken`(user_id, refreshToken) VALUES (:userId, :refreshToken)";
+
+		MapSqlParameterSource param = new MapSqlParameterSource()
+			.addValue("userId", refreshTokenEntity.getUserId())
+			.addValue("refreshToken", refreshTokenEntity.getRefreshToken());
+
+		namedParameterJdbcTemplate.update(sql, param);
+	}
+
+	@Override
+	public List<RefreshTokenEntity> findByUserId(int userId) {
+		String sql = "SELECT * FROM refreshToken WHERE user_id = :userId";
+
+		MapSqlParameterSource param = new MapSqlParameterSource()
+			.addValue("userId", userId);
+
+		return namedParameterJdbcTemplate.query(sql, param, refreshTokenEntityRowMapper());
+	}
+
+	private RowMapper<RefreshTokenEntity> refreshTokenEntityRowMapper() {
+		return BeanPropertyRowMapper.newInstance(RefreshTokenEntity.class);
+	}
+}
