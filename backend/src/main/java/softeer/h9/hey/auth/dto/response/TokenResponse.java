c382a8b43d3380adad5d5895ffa4f8138b9b63d7
윤상현 <yshyeon102@gmail.com>
2023-08-20 05:01:15 +0900

    #129 feat: 토큰 관련 요청에 대한 응답 형식이 맞도록 필드 추가

--- backend/src/main/java/softeer/h9/hey/auth/dto/response/TokenResponse.java
+++ backend/src/main/java/softeer/h9/hey/auth/dto/response/TokenResponse.java
@@ -2,12 +2,11 @@ package softeer.h9.hey.auth.dto.response;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @RequiredArgsConstructor
 public class TokenResponse {
 	private final String accessToken;
 	private final String refreshToken;
+	private final String userName;
 }
