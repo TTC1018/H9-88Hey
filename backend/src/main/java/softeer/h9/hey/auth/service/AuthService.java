50d5405d0270a2623f5932a72f2d61c853b6cfc3
윤상현 <yshyeon102@gmail.com>
2023-08-19 17:09:31 +0900

    #129 feat: Access Token 재발급 기능 구현

--- backend/src/main/java/softeer/h9/hey/auth/service/AuthService.java
+++ backend/src/main/java/softeer/h9/hey/auth/service/AuthService.java
@@ -6,12 +6,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import softeer.h9.hey.auth.domain.RefreshTokenEntity;
 import softeer.h9.hey.auth.domain.User;
+import softeer.h9.hey.auth.dto.request.AccessTokenRequest;
 import softeer.h9.hey.auth.dto.request.JoinRequest;
 import softeer.h9.hey.auth.dto.request.LoginRequest;
 import softeer.h9.hey.auth.dto.response.TokenResponse;
 import softeer.h9.hey.auth.exception.JoinException;
 import softeer.h9.hey.auth.exception.LoginException;
+import softeer.h9.hey.auth.repository.RefreshTokenRepository;
 import softeer.h9.hey.auth.repository.UserRepository;
 
 @Service
@@ -21,13 +24,14 @@ public class AuthService {
 
 	private final JwtTokenProvider jwtTokenProvider;
 	private final UserRepository userRepository;
+	private final RefreshTokenRepository refreshTokenRepository;
 
 	public TokenResponse join(JoinRequest joinRequest) {
 		User user = mapToUser(joinRequest);
 		validateUniqueUserId(user.getUserId());
 
 		user = userRepository.save(user);
-		return getTokenResponse(user);
+		return getTokenResponse(user.getId());
 	}
 
 	private User mapToUser(JoinRequest joinRequest) {
@@ -48,14 +52,15 @@ public class AuthService {
 		User user = validatedUser(userId);
 		checkPassword(password, user);
 
-		return getTokenResponse(user);
+		return getTokenResponse(user.getId());
 	}
 
-	private TokenResponse getTokenResponse(User user) {
-		String userPk = String.valueOf(user.getId());
+	private TokenResponse getTokenResponse(int userId) {
+		String userPk = String.valueOf(userId);
 
 		String accessToken = jwtTokenProvider.generateAccessToken(userPk);
 		String refreshToken = jwtTokenProvider.generateRefreshToken(userPk);
+		refreshTokenRepository.save(new RefreshTokenEntity(Integer.parseInt(userPk), refreshToken));
 
 		return new TokenResponse(accessToken, refreshToken);
 	}
@@ -72,5 +77,11 @@ public class AuthService {
 			throw new LoginException();
 		}
 		return optionalUser.get();
+	}
+
+	public TokenResponse republishAccessToken(AccessTokenRequest accessTokenRequest) {
+		String refreshToken = accessTokenRequest.getRefreshToken();
+		String userId = jwtTokenProvider.getSubjectFromToken(refreshToken);
+		return getTokenResponse(Integer.parseInt(userId));
 	}
 }
