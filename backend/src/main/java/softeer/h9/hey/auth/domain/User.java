4c405354c8986dce5ec3dfe4312cdd52b5d237e2
윤상현 <yshyeon102@gmail.com>
2023-08-19 22:47:59 +0900

    #129 feat: User 클래스의 필드 추가

--- backend/src/main/java/softeer/h9/hey/auth/domain/User.java
+++ backend/src/main/java/softeer/h9/hey/auth/domain/User.java
@@ -11,9 +11,11 @@ public class User {
 	private Integer id;
 	private String userId;
 	private String password;
+	private String name;
 
-	public User(String userId, String password) {
+	public User(String userId, String password, String name) {
 		this.userId = userId;
 		this.password = password;
+		this.name = name;
 	}
 }
