b645b03d9cd59f04877ad3363134e8f9eeb29d6c
윤상현 <yshyeon102@gmail.com>
2023-08-19 13:05:03 +0900

    #129 test: 리프레시 토큰 저장 DAO의 저장, 조회 기능 테스트 추가

---
+++ backend/src/test/java/softeer/h9/hey/auth/repository/RefreshTokenRepositoryTest.java
@@ -0,0 +1,32 @@
+package softeer.h9.hey.auth.repository;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@DisplayName("Refresh Token xㅔ스트")
+class RefreshTokenRepositoryTest {
+
+	@Autowired
+	private RefreshTokenRepository refreshTokenRepository;
+
+	@Test
+	@DisplayName("RefreshToken를 저장 및 조회한다.")
+	void createRefreshTokenTest() {
+		String userId = "userId";
+		String refreshToken = "jwt1234";
+		RefreshTokenEntity refreshTokenEntity = new RefreshTokenEntity(userId, refreshToken);
+
+		refreshTokenRepository.save(refreshTokenEntity);
+		List<RefreshTokenEntity> findRefreshTokenEntities = refreshTokenRepository.findByUserId(userId);
+
+		RefreshTokenEntity findRefreshToken = findRefreshTokenEntities.get(0);
+		Assertions.assertEquals(userId, findRefreshToken.getUserId());
+		Assertions.assertEquals(refreshToken, findRefreshToken.getRefreshToken());
+	}
+}
