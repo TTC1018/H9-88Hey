0b90924dbfa7de993e67bf35e8df632ab7850742
윤상현 <yshyeon102@gmail.com>
2023-08-19 13:50:46 +0900

    #129 feat: RefreshToken Entity 클래스 추가

--- backend/src/test/java/softeer/h9/hey/auth/repository/RefreshTokenRepositoryTest.java
+++ backend/src/test/java/softeer/h9/hey/auth/repository/RefreshTokenRepositoryTest.java
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import softeer.h9.hey.auth.domain.RefreshTokenEntity;
 
 @SpringBootTest
+@Transactional
 @DisplayName("Refresh Token 테스트")
 class RefreshTokenRepositoryTest {
 
