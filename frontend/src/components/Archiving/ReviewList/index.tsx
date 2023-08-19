5d10e70cea38c29184510163de59ff17223f8b11
kim-limit <rlawogks2468@naver.com>
2023-08-19 22:16:30 +0900

    #150 refactor: masonry util 수정

--- frontend/src/components/Archiving/ReviewList/index.tsx
+++ frontend/src/components/Archiving/ReviewList/index.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useLayoutEffect, useRef } from 'react';
 
 import { ArchivingProps } from '@/types/archiving';
-import { masonryLayout } from '@/utils/masonryLayout';
-import { useNavigateWithData } from '@/hooks/useNavigateWithData';
+import { updateMasonryLayout } from '@/utils/updateMasonryLayout';
 
 import { ReviewCard } from '@/components/Archiving/ReviewCard';
 
@@ -16,17 +15,15 @@ interface Props {
 export function ReviewList({ archivings, options, onClick }: Props) {
   const masonryRef = useRef<HTMLDivElement>(null);
 
-  const { naviagteWithData } = useNavigateWithData({ path: '/archiving/detail' });
-
-  useEffect(() => {
-    masonryLayout({ element: masonryRef });
+  useLayoutEffect(() => {
+    updateMasonryLayout({ element: masonryRef });
   }, [archivings]);
 
   return (
     <Styled.Container ref={masonryRef}>
       {archivings.map((archiving, index) => {
         return (
-          <Styled.Wrapper key={index} onClick={() => naviagteWithData({ state: archiving })}>
+          <Styled.Wrapper key={index}>
             <ReviewCard props={archiving} isArchiving={true} selectedSearchOptions={options} onClick={onClick} />
           </Styled.Wrapper>
         );
