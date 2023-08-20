915dc7f7826ff1c15bacfe1d807d6600f9220578
윤상현 <yshyeon102@gmail.com>
2023-08-21 05:59:08 +0900

    #129 feat: Auth 관련 인터셉터 등록

--- backend/src/main/java/softeer/h9/hey/auth/config/AuthWebConfig.java
+++ backend/src/main/java/softeer/h9/hey/auth/config/AuthWebConfig.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
+import softeer.h9.hey.auth.web.interceptor.AuthInterceptor;
 import softeer.h9.hey.auth.web.resolver.LoginUserArgumentResolver;
 
 @Configuration
@@ -14,9 +16,17 @@ import softeer.h9.hey.auth.web.resolver.LoginUserArgumentResolver;
 public class AuthWebConfig implements WebMvcConfigurer {
 
 	private final LoginUserArgumentResolver loginUserArgumentResolver;
+	private final AuthInterceptor authInterceptor;
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(loginUserArgumentResolver);
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authInterceptor)
+			.addPathPatterns("/**")
+			.excludePathPatterns("/auth/**", "/car/**", "/archiving/**");
 	}
 }
