b9d04d0e9404e0be72a7636a961760c9f710a626
윤상현 <yshyeon102@gmail.com>
2023-08-21 06:31:17 +0900

    #129 test: 패스워드 암호화 테스트 코드 버그 수정

--- backend/src/test/java/softeer/h9/hey/auth/service/PasswordEncoderTest.java
+++ backend/src/test/java/softeer/h9/hey/auth/service/PasswordEncoderTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 @DisplayName("Password Encoder 테스트")
 class PasswordEncoderTest {
 
-	private final PasswordEncoder passwordEncoder = new PasswordEncoder();
+	private final PasswordEncoder passwordEncoder = new PasswordEncoder("salt", "SHA-256");
 
 	@Test
 	@DisplayName("패스워드를 인코딩하면 복호화하기 어려운 패스워드로 변환한다.")
@@ -21,7 +21,7 @@ class PasswordEncoderTest {
 
 	@Test
 	@DisplayName("같은 패스워드는 동일하다고 판단한다.")
-	void comparePasswordWithEncodedPassword() {
+	void comparePasswordWithEncodedPasswordSuccessTest() {
 		String password = "12345678";
 		String encodedPassword = passwordEncoder.encode(password);
 
@@ -31,11 +31,11 @@ class PasswordEncoderTest {
 
 	@Test
 	@DisplayName("비밀번호가 다른 경우 패스워드 불일치로 판단한다.")
-	void comparePasswordWithEncodedPassword() {
+	void comparePasswordWithEncodedPasswordFailTest() {
 		String password1 = "12345678";
 		String password2 = "abcdefgh";
 		String encodedPassword = passwordEncoder.encode(password1);
 
-		Assertions.assertTrue(passwordEncoder.compare(password2, encodedPassword));
+		Assertions.assertFalse(passwordEncoder.compare(password2, encodedPassword));
 	}
 }
