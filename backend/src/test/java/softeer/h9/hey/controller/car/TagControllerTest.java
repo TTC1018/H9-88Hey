4272e2c6e96f842d36dd0c2f9382341f49f1a645
ddingmin <ddingminyt@gmail.com>
2023-08-20 16:15:28 +0900

    #173 test: 선택 옵션 id에 따라 상위 limit개의 태그 조회 controller 테스트 작성
    - controller

---
+++ backend/src/test/java/softeer/h9/hey/controller/car/TagControllerTest.java
@@ -0,0 +1,40 @@
+package softeer.h9.hey.controller.car;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+@DisplayName("Tag Controller 테스트")
+@SpringBootTest
+@AutoConfigureMockMvc
+class TagControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	@DisplayName("선택옵션 id에 따라 상위 limit개의 태그를 조회한다.")
+	@Test
+	void getTopTagBySelectOption() throws Exception {
+		String id = "HSS";
+		int limit = 5;
+
+		mockMvc.perform(
+				get("/car/tag/select-option")
+					.param("id", id)
+					.param("limit", String.valueOf(5))
+					.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpectAll(
+				jsonPath("$.statusCode").value(200),
+				jsonPath("$.data.tags").exists(),
+				jsonPath("$.data.tags").isArray()
+			);
+	}
+}
