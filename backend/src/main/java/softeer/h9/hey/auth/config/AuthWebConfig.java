691f73db749393a8d2d524d756aca1f11abe03d4
윤상현 <yshyeon102@gmail.com>
2023-08-21 05:52:01 +0900

    #129 feat: 회원 Argument Resolver 등록

---
+++ backend/src/main/java/softeer/h9/hey/auth/config/AuthWebConfig.java
@@ -0,0 +1,22 @@
+package softeer.h9.hey.auth.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.RequiredArgsConstructor;
+import softeer.h9.hey.auth.web.resolver.LoginUserArgumentResolver;
+
+@Configuration
+@RequiredArgsConstructor
+public class AuthWebConfig implements WebMvcConfigurer {
+
+	private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(loginUserArgumentResolver);
+	}
+}
