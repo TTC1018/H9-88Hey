7ecd2dc36f65b861304a0cd2afd7fe9e660fbed7
ddingmin <ddingminyt@gmail.com>
2023-08-20 16:32:37 +0900

    #173 test: 외장색상 id에 따라 상위 limit개의 태그 조회 service 테스트 작성
    - service

--- backend/src/test/java/softeer/h9/hey/service/car/TagServiceTest.java
+++ backend/src/test/java/softeer/h9/hey/service/car/TagServiceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import softeer.h9.hey.dto.car.request.TagByExteriorColorIdRequest;
 import softeer.h9.hey.dto.car.request.TagBySelectOptionRequest;
 import softeer.h9.hey.dto.car.response.TagResponse;
 import softeer.h9.hey.repository.car.TagRepository;
@@ -32,6 +33,24 @@ class TagServiceTest {
 		when(tagRepository.findTopBySelectOptionId(id, limit)).thenReturn(targetTags);
 
 		TagResponse tagResponse = tagService.findTopBySelectOptionId(request);
+		List<String> tags = tagResponse.getTags();
+
+		assertTrue(limit >= tags.size());
+	}
+
+	@DisplayName("외장색상 id에 따라 상위 limit개의 태그들을 조회한다.")
+	@Test
+	void findTopByExteriorColorId() {
+		int id = 1;
+		int limit = 3;
+		TagByExteriorColorIdRequest request = TagByExteriorColorIdRequest.of(id, limit);
+
+		List<String> targetTags = List.of("멋있어요",
+			"세련된 디자인",
+			"트렌디해요");
+		when(tagRepository.findTopByExteriorColorId(id, limit)).thenReturn(targetTags);
+
+		TagResponse tagResponse = tagService.findTopByExteriorColorId(request);
 		List<String> tags = tagResponse.getTags();
 
 		assertTrue(limit >= tags.size());
