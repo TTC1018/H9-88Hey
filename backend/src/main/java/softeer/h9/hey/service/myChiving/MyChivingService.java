a3b6897fee679fae7a746af7fa866324a81960b3
RecordOfJun <pritty0731@naver.com>
2023-08-21 21:55:27 +0900

    #174 feat: 마이카이빙 정보조회 및 dto 매핑 서비스 코드 구현

--- backend/src/main/java/softeer/h9/hey/service/myChiving/MyChivingService.java
+++ backend/src/main/java/softeer/h9/hey/service/myChiving/MyChivingService.java
@@ -1,13 +1,27 @@
 package softeer.h9.hey.service.myChiving;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import softeer.h9.hey.dto.myChiving.MyChivingDto;
 import softeer.h9.hey.dto.myChiving.MyChivingSaveDto;
+import softeer.h9.hey.dto.myChiving.MyChivingSelectOptionDto;
+import softeer.h9.hey.dto.myChiving.MyChivingSelectOptionFetchDto;
+import softeer.h9.hey.dto.myChiving.request.MyChivingRequest;
 import softeer.h9.hey.dto.myChiving.request.MyChivingSaveRequest;
 import softeer.h9.hey.dto.myChiving.request.MyChivingTempSaveRequest;
 import softeer.h9.hey.dto.myChiving.response.MyChivingIdResponse;
+import softeer.h9.hey.dto.myChiving.response.MyChivingResponse;
+import softeer.h9.hey.dto.myChiving.response.MyChivingsResponse;
 import softeer.h9.hey.repository.myChiving.MyChivingRepository;
 
 @Service
@@ -22,7 +36,9 @@ public class MyChivingService {
 
 		private final boolean value;
 
-		IsSubmit(boolean value) { this.value = value; }
+		IsSubmit(boolean value) {
+			this.value = value;
+		}
 	}
 
 	private final MyChivingRepository myChivingRepository;
@@ -30,12 +46,11 @@ public class MyChivingService {
 	//최종저장
 	public MyChivingIdResponse saveMyCar(final MyChivingSaveRequest myChivingSaveRequest) {
 
-
 		//TODO 인증정보를 통해 사용자 식별 및 user_id 추출
 		int userId = 1;
 		MyChivingSaveDto myChivingSaveDto = MyChivingSaveDto.from(myChivingSaveRequest);
 		myChivingSaveDto.setUserId(userId);
-		myChivingSaveDto.setSubmitted(IsSubmit.SUBMIT.value );
+		myChivingSaveDto.setSubmitted(IsSubmit.SUBMIT.value);
 
 		Long myChivingId = myChivingRepository.saveMyCarToMyChiving(myChivingSaveDto);
 
@@ -48,11 +63,106 @@ public class MyChivingService {
 		int userId = 1;
 		MyChivingSaveDto myChivingSaveDto = MyChivingSaveDto.from(myChivingSaveRequest);
 		myChivingSaveDto.setUserId(userId);
-		myChivingSaveDto.setSubmitted(IsSubmit.TEMPORARY_SUBMIT.value );
+		myChivingSaveDto.setSubmitted(IsSubmit.TEMPORARY_SUBMIT.value);
 
 		Long myChivingId = myChivingRepository.saveMyCarToMyChiving(myChivingSaveDto);
 
 		return MyChivingIdResponse.from(myChivingId);
 	}
 
+	public MyChivingsResponse findMyChvings(final MyChivingRequest myChivingRequest) {
+		int limit = myChivingRequest.getLimit();
+		int startIndex = (myChivingRequest.getOffset() - 1) * limit;
+
+		//TODO 아이디 필요
+		List<MyChivingDto> myChivingDtoList = myChivingRepository.findMyChivingsByUserIdLimitAndOffset(1, limit,
+			startIndex);
+
+		return MyChivingsResponse.from(test(myChivingDtoList));
+	}
+
+	private List<MyChivingResponse> test(List<MyChivingDto> myChivingDtoList) {
+		List<Long> myChivingIdList = myChivingDtoList.stream()
+			.map(MyChivingDto::getMyChivingId)
+			.collect(Collectors.toList());
+
+		List<MyChivingSelectOptionFetchDto> myChivingSelectOptionFetchDtoList = myChivingRepository.findSelectOptionIdByMyChivingIdList(
+			myChivingIdList);
+
+		Map<String, Set<String>> subOptionMap = new HashMap<>();
+		Map<Long, Set<String>> selectOptionMap = new HashMap<>();
+		Map<String, MyChivingSelectOptionDto> selectOptionContentMap = new HashMap<>();
+
+		for (MyChivingSelectOptionFetchDto myChivingSelectOptionFetchDto : myChivingSelectOptionFetchDtoList) {
+			//선택옵션에 해당하는 하위옵션을 넣기 위한 코드, 처음에 집합 초기화
+			initSubOptionData(subOptionMap, myChivingSelectOptionFetchDto.getSelectOptionId(),
+				myChivingSelectOptionFetchDto.getSubOptionName());
+			//마이카이빙에서 가지고있는 선택 옵션 매핑
+			initSelectOptionData(selectOptionMap, myChivingSelectOptionFetchDto.getMyChivingId(),
+				myChivingSelectOptionFetchDto.getSelectOptionId());
+		}
+
+		//선택 옵션이 가지고 있는 정보 매핑
+		for (MyChivingSelectOptionFetchDto myChivingSelectOptionFetchDto : myChivingSelectOptionFetchDtoList) {
+			initSelectOptionContentMap(selectOptionContentMap, subOptionMap, myChivingSelectOptionFetchDto);
+		}
+
+		return mapToMyChivingResponse(myChivingDtoList, selectOptionMap, selectOptionContentMap);
+	}
+
+	private void initSubOptionData(Map<String, Set<String>> subOptionMap, String selectOptionId, String subOptionName) {
+		if (!subOptionMap.containsKey(selectOptionId)) {
+			subOptionMap.put(selectOptionId, new HashSet<>());
+		}
+
+		subOptionMap.get(selectOptionId).add(subOptionName);
+	}
+
+	private void initSelectOptionData(Map<Long, Set<String>> selectOptionMap, Long myChivingId, String selectOptionId) {
+		if (!selectOptionMap.containsKey(myChivingId)) {
+			selectOptionMap.put(myChivingId, new HashSet<>());
+		}
+
+		selectOptionMap.get(myChivingId).add(selectOptionId);
+	}
+
+	private void initSelectOptionContentMap(Map<String, MyChivingSelectOptionDto> selectOptionContentMap,
+		Map<String, Set<String>> subOptionMap,
+		MyChivingSelectOptionFetchDto myChivingSelectOptionFetchDto) {
+		if (!selectOptionContentMap.containsKey(myChivingSelectOptionFetchDto.getSelectOptionId())) {
+			MyChivingSelectOptionDto myChivingSelectOptionDto =
+				MyChivingSelectOptionDto.builder()
+					.id(myChivingSelectOptionFetchDto.getSelectOptionId())
+					.name(myChivingSelectOptionFetchDto.getSelectOptionName())
+					.imageUrl(myChivingSelectOptionFetchDto.getImageUrl())
+					.additionalPrice(myChivingSelectOptionFetchDto.getAdditionalPrice())
+					.subOptions(subOptionMap.get(myChivingSelectOptionFetchDto.getSelectOptionId())
+						.stream().collect(Collectors.toList()))
+					.build();
+
+			selectOptionContentMap.put(myChivingSelectOptionFetchDto.getSelectOptionId(),
+				myChivingSelectOptionDto);
+		}
+	}
+
+	private List<MyChivingResponse> mapToMyChivingResponse(List<MyChivingDto> myChivingDtoList,
+		Map<Long, Set<String>> selectOptionMap, Map<String, MyChivingSelectOptionDto> selectOptionContentMap) {
+
+		List<MyChivingResponse> myChivingResponseList = new ArrayList<>();
+		myChivingDtoList.stream()
+			.forEach(myChivingDto -> {
+				List<MyChivingSelectOptionDto> selectOptionDtoList = new ArrayList<>();
+
+				if (selectOptionMap.containsKey(myChivingDto.getMyChivingId())) {
+					selectOptionMap.get(myChivingDto.getMyChivingId()).stream()
+						.forEach(selectOptionId -> {
+							selectOptionDtoList.add(selectOptionContentMap.get(selectOptionId));
+						});
+				}
+
+				myChivingResponseList.add(MyChivingResponse.of(myChivingDto, selectOptionDtoList));
+			});
+
+		return myChivingResponseList;
+	}
 }
