be0029a4a00f7e592f92198acac4490a06247b9b
RecordOfJun <pritty0731@naver.com>
2023-08-21 13:03:30 +0900

    #174 feat: 마이카이빙 조회용 하위옵션 매핑 dto 구성

---
+++ backend/src/main/java/softeer/h9/hey/dto/myChiving/MyChivingSubOptionDto.java
@@ -0,0 +1,14 @@
+package softeer.h9.hey.dto.myChiving;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MyChivingSubOptionDto {
+	private String id;
+	private String name;
+}
