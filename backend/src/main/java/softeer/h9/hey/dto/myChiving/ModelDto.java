9f659e3d6ff1c50c608d74a1fd7fc9c3b315abae
RecordOfJun <pritty0731@naver.com>
2023-08-21 13:04:51 +0900

    #174 feat: 모델 정보 dto 생성

---
+++ backend/src/main/java/softeer/h9/hey/dto/myChiving/ModelDto.java
@@ -0,0 +1,21 @@
+package softeer.h9.hey.dto.myChiving;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import softeer.h9.hey.dto.archiving.TrimDto;
+
+@Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ModelDto {
+	private Integer id;
+	private String name;
+
+	public static ModelDto of(final Integer id, final String name) {
+		return new ModelDto(id, name);
+	}
+}
