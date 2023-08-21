0f15ba50917179748f24158d9c45fdfe1f00f3d7
RecordOfJun <pritty0731@naver.com>
2023-08-21 21:57:24 +0900

    #174 feat: 마이카이빙 조회 컨트롤러 구현

--- backend/src/main/java/softeer/h9/hey/controller/myChiving/MyChivingController.java
+++ backend/src/main/java/softeer/h9/hey/controller/myChiving/MyChivingController.java
@@ -1,15 +1,21 @@
 package softeer.h9.hey.controller.myChiving;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import softeer.h9.hey.dto.myChiving.MyChivingDto;
+import softeer.h9.hey.dto.myChiving.request.MyChivingRequest;
 import softeer.h9.hey.dto.myChiving.request.MyChivingSaveRequest;
 import softeer.h9.hey.dto.myChiving.request.MyChivingTempSaveRequest;
 import softeer.h9.hey.dto.myChiving.response.MyChivingIdResponse;
+import softeer.h9.hey.dto.myChiving.response.MyChivingsResponse;
 import softeer.h9.hey.service.myChiving.MyChivingService;
 import softeer.h9.hey.dto.global.response.GlobalResponse;
 
@@ -29,5 +35,11 @@ public class MyChivingController {
 	public GlobalResponse<MyChivingIdResponse> temporarySaveMyCarToMyChiving(@Valid @RequestBody MyChivingTempSaveRequest myChivingSaveRequest) {
 		MyChivingIdResponse response = myChivingService.temporarySaveMyCar(myChivingSaveRequest);
 		return GlobalResponse.ok(response);
+	}
+
+	@GetMapping("/mychiving")
+	public GlobalResponse<MyChivingsResponse> findMyChivings(final MyChivingRequest myChivingRequest) {
+		MyChivingsResponse myChivingsResponse = myChivingService.findMyChvings(myChivingRequest);
+		return GlobalResponse.ok(myChivingsResponse);
 	}
 }
