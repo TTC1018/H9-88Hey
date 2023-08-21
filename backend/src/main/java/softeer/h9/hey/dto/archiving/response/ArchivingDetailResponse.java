0d62dcd7aac3595cd296283b4da91c23d71d8151
ddingmin <ddingminyt@gmail.com>
2023-08-21 14:59:11 +0900

    #183 feat: 아카이빙 상세 조회 response 구현

---
+++ backend/src/main/java/softeer/h9/hey/dto/archiving/response/ArchivingDetailResponse.java
@@ -0,0 +1,37 @@
+package softeer.h9.hey.dto.archiving.response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import softeer.h9.hey.dto.archiving.BodyTypeDto;
+import softeer.h9.hey.dto.archiving.EngineDto;
+import softeer.h9.hey.dto.archiving.ExteriorColorDto;
+import softeer.h9.hey.dto.archiving.InteriorColorDto;
+import softeer.h9.hey.dto.archiving.SelectOptionDto;
+import softeer.h9.hey.dto.archiving.TrimDto;
+import softeer.h9.hey.dto.archiving.WheelDriveDto;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class ArchivingDetailResponse {
+	private Long feedId;
+	private String modelName;
+	private String creationDate;
+	private boolean isPurchase;
+	private String review;
+	private Integer totalPrice;
+	private TrimDto trim;
+	private EngineDto engine;
+	private BodyTypeDto bodyType;
+	private WheelDriveDto wheelDrive;
+	private InteriorColorDto interiorColor;
+	private ExteriorColorDto exteriorColor;
+	private List<SelectOptionDto> selectedOptions = new ArrayList<>();
+}
