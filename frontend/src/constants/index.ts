cad32b331418014c3ab75e07ea4972044bf529f7
Kim jae han <rlawogks2468@naver.com>
2023-08-21 22:15:13 +0900

    hotfix: api url 수정

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -7,7 +7,7 @@ export enum ModalType {
   CLEAR = 'CLEAR',
 }
 export const TAG_CHIP_MAX_NUMBER = 3;
-export const API_URL = import.meta.env.VITE_API_URL;
+export const API_URL = 'http://api.88hey.site:8080';
 export const NAVIGATION_PATH = {
   '/trim': { prev: '', next: '/trim/engine' },
   '/trim/engine': { prev: '/trim', next: '/trim/body-type' },
