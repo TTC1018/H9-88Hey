57cb134e4efea4049ece03c7eda8896742efa278
ddingmin <ddingminyt@gmail.com>
2023-08-23 01:52:46 +0900

    #204 feat: 북마크 추가 service 구현

--- backend/src/main/java/softeer/h9/hey/service/myChiving/FeedService.java
+++ backend/src/main/java/softeer/h9/hey/service/myChiving/FeedService.java
@@ -3,9 +3,11 @@ package softeer.h9.hey.service.myChiving;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
+import softeer.h9.hey.controller.myChiving.ArchivingIdRequest;
 import softeer.h9.hey.domain.archiving.Feed;
 import softeer.h9.hey.dto.myChiving.request.BookmarkRequest;
 import softeer.h9.hey.dto.myChiving.response.BookmarkResponse;
+import softeer.h9.hey.dto.myChiving.response.FeedIdResponse;
 import softeer.h9.hey.repository.myChiving.FeedRepository;
 
 @Service
@@ -13,12 +15,15 @@ import softeer.h9.hey.repository.myChiving.FeedRepository;
 public class FeedService {
 	private final FeedRepository repository;
 
-	public BookmarkResponse hasBookmark(
-		final int userId,
-		final BookmarkRequest request) {
-
+	public BookmarkResponse hasBookmark(final int userId, final BookmarkRequest request) {
 		Feed result = repository.hasBookmark(userId, request.getFeedId());
 
 		return BookmarkResponse.of(result.getIsMarked());
+	}
+
+	public FeedIdResponse saveFeed(final int userId, final ArchivingIdRequest request) {
+		int result = repository.save(userId, request.getFeedId());
+
+		return FeedIdResponse.of(request.getFeedId());
 	}
 }
