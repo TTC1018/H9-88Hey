0acb8a8a47b6623a8c94e05c2cc3eff885ee9a4b
ddingmin <ddingminyt@gmail.com>
2023-08-22 21:31:03 +0900

    #204 feat: 북마크 조회 controller 구현

---
+++ backend/src/main/java/softeer/h9/hey/controller/myChiving/FeedController.java
@@ -0,0 +1,28 @@
+package softeer.h9.hey.controller.myChiving;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import softeer.h9.hey.auth.annotation.LoginUser;
+import softeer.h9.hey.dto.global.response.GlobalResponse;
+import softeer.h9.hey.dto.myChiving.request.BookmarkRequest;
+import softeer.h9.hey.dto.myChiving.response.BookmarkResponse;
+import softeer.h9.hey.service.myChiving.FeedService;
+
+@RestController
+@RequiredArgsConstructor
+public class FeedController {
+
+	private final FeedService service;
+
+		@GetMapping("/mychiving/detail/bookmark")
+	public GlobalResponse<BookmarkResponse> getBookmarkByArchivingId(
+		@LoginUser int userId,
+		BookmarkRequest bookmarkRequest) {
+
+		BookmarkResponse response = service.hasBookmark(userId, bookmarkRequest);
+
+		return GlobalResponse.ok(response);
+	}
+}
