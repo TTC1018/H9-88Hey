bbfabd974887f15fa253ff8ac4d25c7e9e630ae3
ttc1018 <paramore@kakao.com>
2023-08-22 22:21:23 +0900

    #220 feat: 아카이빙 바텀바 총가격 데이터 적용

--- android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/ui/component/BottomBars.kt
+++ android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/ui/component/BottomBars.kt
@@ -170,10 +170,11 @@ fun MakeCarBottomBar(
 @Composable
 fun ArchiveBottomBar(
     modifier: Modifier = Modifier,
+    totalPrice: Int,
 ) {
     BottomBar(
         modifier = modifier,
-        totalPrice = 0,
+        totalPrice = totalPrice,
         summaryText = stringResource(id = R.string.archive_total_price),
         underLineWidth = 35,
         buttonArea = {
@@ -221,7 +222,7 @@ fun MyArchiveDetailBottomBar(
 ) {
     when (screenIndex) {
         MY_ARCHIVE_MADE -> MyArchiveBottomBar(modifier = modifier)
-        MY_ARCHIVE_SAVE -> ArchiveBottomBar(modifier = modifier)
+        MY_ARCHIVE_SAVE -> ArchiveBottomBar(modifier = modifier, totalPrice = 0)
     }
 }
 
@@ -238,7 +239,7 @@ fun PreviewMakeCarBottomBar() {
 @Preview
 @Composable
 fun PreviewArchiveBottomBar() {
-    ArchiveBottomBar()
+    ArchiveBottomBar(totalPrice = 0)
 }
 
 @Preview
