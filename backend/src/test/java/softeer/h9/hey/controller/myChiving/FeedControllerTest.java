0542546fac2011605736e3e9f661d63dc2a0937c
ddingmin <ddingminyt@gmail.com>
2023-08-23 02:07:44 +0900

    #204 test: 북마크 추가 controller 테스트 작성

--- backend/src/test/java/softeer/h9/hey/controller/myChiving/FeedControllerTest.java
+++ backend/src/test/java/softeer/h9/hey/controller/myChiving/FeedControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -29,7 +30,21 @@ class FeedControllerTest {
 				.param("feed_id", String.valueOf(479893076429349285L)))
 			.andExpect(MockMvcResultMatchers.status().isOk())
 			.andExpect(MockMvcResultMatchers.jsonPath("$.data.bookmark").value(true));
-
 	}
 
+	@DisplayName("유저가 아카이빙 ID를 통해 해당하는 북마크를 저장한다.")
+	@Test
+	@Transactional
+	void saveFeedByArchivingId() throws Exception {
+		long feedId = 479893076429349285L;
+		String requestBody = "{\"feed_id\":" + feedId + "}";
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/mychiving/feed")
+			.contentType("application/json")
+			.header("Authorization",
+				"Bearer eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiIxIiwidXNlck5hbWUiOiJ0ZXN0IiwiaWF0IjoxNjkyNTYwMzM5LCJleHAiOjQ4MTQ2MjQzMzl9.gcSE7kPaRVxo2iT9DRcN1Bn5ZNAAsHG8Z3dvTopH-IWblMf_LJ2lhsYqOvrrLcZJ")
+			.content(requestBody))
+			.andExpect(MockMvcResultMatchers.status().isOk())
+			.andExpect(MockMvcResultMatchers.jsonPath("$.data.feedId").value(feedId));
+	}
 }
