a535ba499e7a56ca39a2c83146ccfb3923b7ae0a
RecordOfJun <pritty0731@naver.com>
2023-08-22 17:04:31 +0900

    #211 feat: MyChivingsResponse nextOffset 추가

--- backend/src/main/java/softeer/h9/hey/dto/myChiving/response/MyChivingsResponse.java
+++ backend/src/main/java/softeer/h9/hey/dto/myChiving/response/MyChivingsResponse.java
@@ -2,10 +2,12 @@ package softeer.h9.hey.dto.myChiving.response;
 
 import java.util.List;
 
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.Getter;
 
 @Getter
 public class MyChivingsResponse {
+	private Integer nextOffset;
 	private List<MyChivingResponse> myChivings;
 
 	private MyChivingsResponse(List<MyChivingResponse> myChivings) {
