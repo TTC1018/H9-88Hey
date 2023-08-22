399ca7fb4ca8c3b4846228b9e255bce23203774a
DongHyeon Park <paramore@kakao.com>
2023-08-22 19:21:21 +0900

    #103 feat: 아카이빙 상세화면 UiModel 및 Mapper 선언

---
+++ android/MyCarchiving/app/src/main/java/com/softeer/mycarchiving/model/common/CarDetailsUiModels.kt
@@ -0,0 +1,23 @@
+package com.softeer.mycarchiving.model.common
+
+import com.softeer.mycarchiving.model.makingcar.ColorOptionSimpleUiModel
+
+data class CarDetailsUiModel(
+    val id: Long,
+    val model: String,
+    val trim: String,
+    val trimOptions: List<String>,
+    val interiorColor: ColorOptionSimpleUiModel,
+    val exteriorColor: ColorOptionSimpleUiModel,
+    val selectedOptions: List<CarDetailSelectOptionUiModel>? = null,
+    val review: String? = null,
+)
+
+data class CarDetailSelectOptionUiModel(
+    val id: String,
+    val name: String,
+    val imageUrl: String,
+    val reviewText: String,
+    val subOptions: List<String>? = null,
+    val tags: List<String>? = null
+)
