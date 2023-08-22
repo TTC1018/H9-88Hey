eba312c79c5ed4962a7566f948e921a694356df1
ddingmin <ddingminyt@gmail.com>
2023-08-23 01:43:45 +0900

    #204 feat: 북마크 추가 request dto 구현

---
+++ backend/src/main/java/softeer/h9/hey/controller/myChiving/ArchivingIdRequest.java
@@ -0,0 +1,20 @@
+package softeer.h9.hey.controller.myChiving;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+
+@Getter
+public class ArchivingIdRequest {
+	private Long feedId;
+
+	@JsonCreator
+	private ArchivingIdRequest(@JsonProperty("feed_id") Long feed_id) {
+		this.feedId = feed_id;
+	}
+
+	public static ArchivingIdRequest from(Long feedId) {
+		return new ArchivingIdRequest(feedId);
+	}
+}
