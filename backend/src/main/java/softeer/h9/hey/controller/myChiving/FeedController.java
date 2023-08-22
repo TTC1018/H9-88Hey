7f2244051f91deaad274454fe7432e674b7d9d37
ddingmin <ddingminyt@gmail.com>
2023-08-23 02:07:55 +0900

    #204 test: 북마크 추가 controller 구현

--- backend/src/main/java/softeer/h9/hey/controller/myChiving/FeedController.java
+++ backend/src/main/java/softeer/h9/hey/controller/myChiving/FeedController.java
@@ -1,6 +1,8 @@
 package softeer.h9.hey.controller.myChiving;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,7 @@ import softeer.h9.hey.auth.annotation.LoginUser;
 import softeer.h9.hey.dto.global.response.GlobalResponse;
 import softeer.h9.hey.dto.myChiving.request.BookmarkRequest;
 import softeer.h9.hey.dto.myChiving.response.BookmarkResponse;
+import softeer.h9.hey.dto.myChiving.response.FeedIdResponse;
 import softeer.h9.hey.service.myChiving.FeedService;
 
 @RestController
@@ -24,5 +27,13 @@ public class FeedController {
 		BookmarkResponse response = service.hasBookmark(userId, bookmarkRequest);
 
 		return GlobalResponse.ok(response);
+	}
+
+	@PostMapping("/mychiving/feed")
+	public GlobalResponse<FeedIdResponse> saveArchivingFeed(
+		@LoginUser int userId,
+		@RequestBody ArchivingIdRequest archivingIdRequest) {
+		FeedIdResponse result = service.saveFeed(userId, archivingIdRequest);
+		return GlobalResponse.ok(result);
 	}
 }
