3817f3d9a0b598ea857e6615caf042672c697d44
윤상현 <yshyeon102@gmail.com>
2023-08-23 16:45:46 +0900

    #229 feat: 토큰 유효성 검증의 응답 클래스 UserNameResponse 추가

---
+++ backend/src/main/java/softeer/h9/hey/auth/dto/response/UserNameResponse.java
@@ -0,0 +1,10 @@
+package softeer.h9.hey.auth.dto.response;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class UserNameResponse {
+	private final String userName;
+}
