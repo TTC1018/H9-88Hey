d7c697b1324217b43f0ca5e05407eee060a69411
ddingmin <ddingminyt@gmail.com>
2023-08-23 09:30:58 +0900

    #204 test: 미리 데이터를 주입하는 방식으로 테스트 방식 변경

--- backend/src/test/java/softeer/h9/hey/repository/myChiving/FeedRepositoryTest.java
+++ backend/src/test/java/softeer/h9/hey/repository/myChiving/FeedRepositoryTest.java
@@ -2,6 +2,9 @@ package softeer.h9.hey.repository.myChiving;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,35 +15,38 @@ import softeer.h9.hey.domain.archiving.Feed;
 
 @SpringBootTest
 @DisplayName("Feed Repository 테스트")
+@Transactional
 class FeedRepositoryTest {
 
 	@Autowired
 	private FeedRepository repository;
 
+	private final int testUserId = 0;
+	private final long testFeedId = 12345L;
+
+	@BeforeEach
+	void setUp() {
+		repository.save(testUserId, testFeedId);
+	}
+
 	@DisplayName("북마크가 존재하는지 확인한다.")
 	@Test
 	void hasBookmark() {
-		Long feedId = 479893076429349285L;
-		int userId = 1;
+		List<Feed> feeds = repository.hasBookmark(testUserId, testFeedId);
 
-		Feed feed = repository.hasBookmark(userId, feedId);
-
-		assertAll(
-			() -> assertEquals(feedId, feed.getFeedId()),
-			() -> assertEquals(userId, feed.getUserId()),
-			() -> assertEquals(Boolean.TRUE, feed.getIsMarked())
-		);
+		assertEquals(1, feeds.size());
 	}
 
 	@Test
 	@DisplayName("유저가 아카이빙 피드를 북마크한다.")
-	@Transactional
 	void save() {
-		Long feedId = 1234567890L;
+		long feedId = 1234567890L;
 		int userId = 1;
 
 		int save = repository.save(userId, feedId);
+		List<Feed> feeds = repository.hasBookmark(userId, userId);
 
 		assertEquals(1, save);
+		assertTrue(feeds.size() > 0);
 	}
 }
