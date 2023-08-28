8f62605f7b2276d60c814f6ca3ceb006cd42cd66
sukam09 <sukam09@naver.com>
2023-08-28 14:32:08 +0900

    #275 feat: 에러 바운더리 메시지 및 스타일 변경

--- frontend/src/components/common/ErrorBoundary/index.tsx
+++ frontend/src/components/common/ErrorBoundary/index.tsx
@@ -34,9 +34,9 @@ export const ErrorBoundary = Catch(function ErrorBoundary(props: Props, error?: 
       <Styled.Container>
         <Styled.Wrapper>
           <Styled.Image src={'https://www.hyundai.com/static/images/logo.png'} />
-          {error instanceof AuthError && error.statusCode === 401 ? (
+          {error instanceof AuthError && (error.statusCode === 400 || error.statusCode === 401) ? (
             <>
-              <Styled.Body>로그인 상태가 아니거나 세션이 만료되었습니다.</Styled.Body>
+              <Styled.Head>로그인 상태가 아니거나 세션이 만료되었습니다.</Styled.Head>
               <Styled.Body>로그인 후 다시 시도해주세요.</Styled.Body>
               <Styled.Button href="/">로그인 페이지로 이동</Styled.Button>
             </>
@@ -45,7 +45,7 @@ export const ErrorBoundary = Catch(function ErrorBoundary(props: Props, error?: 
               <Styled.Head>현대닷컴 접속이 원활하지 않습니다.</Styled.Head>
               <Styled.Body>일시적인 현상이거나, 네트워크 문제일 수 있으니</Styled.Body>
               <Styled.Body>잠시 후 다시 시도해주세요.</Styled.Body>
-              <Styled.Button href="/trim">트림 선택 페이지로 이동</Styled.Button>
+              <Styled.Button href="/trim">메인 페이지로 이동</Styled.Button>
             </>
           )}
         </Styled.Wrapper>
