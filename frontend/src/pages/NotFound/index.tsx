7c051266c3c9a1cbd8ab8e94d638c657279a5fc8
sukam09 <sukam09@naver.com>
2023-08-28 14:31:29 +0900

    #275 feat: 404 Not Found 문구 추가

--- frontend/src/pages/NotFound/index.tsx
+++ frontend/src/pages/NotFound/index.tsx
@@ -5,7 +5,7 @@ import * as Styled from './style';
 export function NotFound() {
   return (
     <Styled.Container>
-      <EmptyContent text="요청하신 페이지를 찾을 수 없어요" />
+      <EmptyContent text="요청하신 페이지를 찾을 수 없어요" isNotFound />
     </Styled.Container>
   );
 }
